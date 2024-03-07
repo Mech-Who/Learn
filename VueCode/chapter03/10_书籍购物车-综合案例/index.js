607f43757fabb37d0ac5ca95005e7257ca503260
hushuhan <873933169@qq.com>
2024-03-07 23:29:02 +0800

    实现加购物车功能

--- VueCode/chapter03/10_书籍购物车-综合案例/index.js
+++ VueCode/chapter03/10_书籍购物车-综合案例/index.js
@@ -1,43 +1,57 @@
 Vue.createApp({
-    template: "#my-app",
-    data() {
-        return {
-            books: [
-                {
-                    id: 1,
-                    name: '《算法导论》',
-                    date: '2006-9',
-                    price: 85.00,
-                    count: 1
-                },
-                {
-                    id: 2,
-                    name: '《UNIX编程艺术》',
-                    date: '2006-2',
-                    price: 59.00,
-                    count: 1
-                },
-                {
-                    id: 3,
-                    name: '《编程珠玑》',
-                    date: '2008-10',
-                    price: 39.00,
-                    count: 1
-                },
-                {
-                    id: 4,
-                    name: '《代码大全》',
-                    date: '2006-3',
-                    price: 128.00,
-                    count: 1
-                },
-            ]
-        }
+  template: "#my-app",
+  data() {
+    return {
+      books: [
+        {
+          id: 1,
+          name: "《算法导论》",
+          date: "2006-9",
+          price: 85.0,
+          count: 1,
+        },
+        {
+          id: 2,
+          name: "《UNIX编程艺术》",
+          date: "2006-2",
+          price: 59.0,
+          count: 1,
+        },
+        {
+          id: 3,
+          name: "《编程珠玑》",
+          date: "2008-10",
+          price: 39.0,
+          count: 1,
+        },
+        {
+          id: 4,
+          name: "《代码大全》",
+          date: "2006-3",
+          price: 128.0,
+          count: 1,
+        },
+      ],
+    };
+  },
+  computed: {
+    totalPrice() {
+      let finalPrice = 0;
+      for (let book of this.books) {
+        finalPrice += book.price * book.count;
+      }
+      return finalPrice;
     },
-    computed: {
-            
+  },
+  methods: {
+    decrement(index) {
+      this.books[index].count--;
     },
-    methods: {
-
-    }
+    increment(index) {
+      this.books[index].count++;
+    },
+    removeBook(index) {
+      this.books.splice(index, 1);
+    },
+  },
 }).mount("#app");
