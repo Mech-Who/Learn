45846cd15e425846007500b166e8e78655aff009
hushuhan <873933169@qq.com>
2024-03-10 21:44:48 +0800

    learn vue-cli's config file: vue.config.js

--- VueCode/chapter06/01_vuecli_demo/vue.config.js
+++ VueCode/chapter06/01_vuecli_demo/vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
+const { defineConfig } = require("@vue/cli-service");
+function resolve(dir) {
+  return path.join(__dirname, dir); // __dirname获取当前问价按所在的路径
+}
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true, // 如果选择true，那么项目引用node_modules中的包也会用Bable来编译
+  outputDir: "build",
+  assetsDir: "static",
+  publicPath: process.env.NODE_ENV === "production" ? "/my-app/" : "/",
+  chainWebpack: (config) => {
+    // 1. 为路径起别名
+    config.resolve.alias
+      // 2. @别名代表的路径是：指向当前项目的src目录
+      .set("@", resolve("src"))
+      // 3. components别名代表的路径是：指向当前项目src目录下的components目录
+      .set("components", resolve("src/components"));
+  },
+});
