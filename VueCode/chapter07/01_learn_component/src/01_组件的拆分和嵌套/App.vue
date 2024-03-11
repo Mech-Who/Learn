799f199c99d5f8b16f31432eefa4f16153403fdc
hushuhan <873933169@qq.com>
2024-03-12 00:27:18 +0800

    组件化分离

---
+++ VueCode/chapter07/01_learn_component/src/01_组件的拆分和嵌套/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <!-- 3. 使用注册的组件 -->
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+// 1. 分别倒入Header、Main和Footer组件
+import Header from "./Header.vue";
+// 导入文件可以省略扩展名，但建议不要省略。省略扩展名后，在模板中使用该组件时，将不会有智能提示
+import Main from "./Main";
+import Footer from "./Footer";
+export default {
+  components: {
+    // 2. 注册局部组件
+    Header,
+    Main,
+    Footer,
+  },
+};
+</script>
---
+++ VueCode/chapter07/01_learn_component/src/01_组件的拆分和嵌套/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    <!-- 4. 使用局部注册的组件，也支持<MainBanner></MainBanner>这种语法 -->
+    <main-banner></main-banner>
+    <main-product-list></main-product-list>
+  </div>
+</template>
+<script>
+// 1. 导入轮播图组件
+import MainBanner from "./MainBanner.vue";
+// 2. 导入商品列表组件
+import MainProductList from "./MainProductList.vue";
+export default {
+  components: {
+    // 3.注册局部组件
+    MainBanner, // 等价于MainBanner: MainBanner写法
+    MainProductList,
+  },
+};
+</script>
