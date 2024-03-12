025f1da1d7f91f1414993e7212f8fda6f8394ad1
hushuhan <873933169@qq.com>
2024-03-12 22:26:02 +0800

    局部样式泄露

---
+++ VueCode/chapter07/01_learn_component/src/02_组件style特性/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h4>App Title</h4>
+  <hello-world></hello-world>
+</template>
+
+<script>
+import HelloWorld from "./HelloWorld.vue";
+export default {
+    components: {
+        HelloWorld
+    }
+};
+</script>
+
+<style scoped>
+h4 {
+    text-decoration: underline;
+}
+</style>
--- VueCode/chapter07/01_learn_component/src/main.js
+++ VueCode/chapter07/01_learn_component/src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './01_组件的拆分和嵌套/App.vue'
+import App from './02_组件style特性/App.vue'
 
 createApp(App).mount('#app')
