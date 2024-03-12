c8aa8ceeb83b3d3d895b29afdd8deb6ced68bdbb
hushuhan <873933169@qq.com>
2024-03-12 23:10:58 +0800

    props为字符串类型的数组

---
+++ VueCode/chapter07/01_learn_component/src/03_父组件传递子组件/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="app">
+        <!-- 1. 直接传递字符串 -->
+        <show-message title="我是标题" content="我是内容"></show-message>
+        <!-- 2. 绑定字符串类型的变量 -->
+        <show-message :title="title" :content="content"></show-message>
+        <!-- 3. 绑定对象中字符串类型的属性 -->
+        <show-message :title="message.title" :content="message.content"></show-message>
+        <!-- 4. 直接绑定一个对象，会自动将对象的每个属性拆出来逐一绑定 -->
+        <show-message v-bind="message"></show-message>
+    </div>
+</template>
+
+<script>
+    import ShowMessage from './ShowMessage.vue'
+    export default {
+        components: {
+            ShowMessage
+        }
+    },
+    data() {
+        return {
+            title: "我是标题title",
+            content: "我是内容content",
+            message: {
+                title: "我是标题message.title",
+                content: "我是内容message.content"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ VueCode/chapter07/01_learn_component/src/03_父组件传递子组件/ShowMessage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="show-message">
+        <h4>{{ title }}</h4>
+        <div>{{ content }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['title', 'content']
+    }
+</script>
+
+<style scoped>
+</style>
