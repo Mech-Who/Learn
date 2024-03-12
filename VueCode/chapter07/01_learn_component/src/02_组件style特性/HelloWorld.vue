8a81c0ee669ccd465218339cbb7d10c4e6fa3166
hushuhan <873933169@qq.com>
2024-03-12 22:38:36 +0800

    样式泄露的解决方案之一——多个根元素

--- VueCode/chapter07/01_learn_component/src/02_组件style特性/HelloWorld.vue
+++ VueCode/chapter07/01_learn_component/src/02_组件style特性/HelloWorld.vue
@@ -1,10 +1,17 @@
+<!-- 将根元素换成div，顺便为根元素添加一个class类。这个类暂时并未用到，只是推荐这样写 -->
 <template>
+  <div class="hello-world">
     <h4>Hello World1</h4>
+  </div>
+</template>
+
+<!-- 多个根元素 -->
+<template>
+  <h4>Hello World1</h4>
 </template>
 
 <script>
-    export default {}
+export default {};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
