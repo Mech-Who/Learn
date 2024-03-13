8f74a3e00936aac0b9d65e3aa367ba33b58a66f1
hushuhan <873933169@qq.com>
2024-03-13 22:27:36 +0800

    自定义事件流程

---
+++ VueCode/chapter07/01_learn_component/src/04_子组件传递父组件/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="app">
+        <h4>当前计数： {{ counter }}</h4>
+        <counter-operation @add="addOne" @sub="subOne"/>
+    </div>
+</template>
+
+<script>
+    import CounterOperation from './CounterOperation.vue'
+    export default {
+        components: {
+            CounterOperation
+        },
+        data() {
+            return {
+                counter: 0
+            }
+        },
+        methods: {
+            addOne() {
+                this.counter++
+            },
+            subOne() {
+                this.counter--
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ VueCode/chapter07/01_learn_component/src/04_子组件传递父组件/CounterOperation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emit: ["add", "sub"], // 1. 定义该组件可以向其父组件出发的add和sub事件
+  methods: {
+    increment() {
+      this.$emit("add"); // 2. 触发自定义add事件，$emit可以接收多个参数，其中第一个参数是事件名称
+    },
+    decrement() {
+      this.$emit("sub"); // 3. 触发自定义sub事件
+    },
+  },
+};
+</script>
+
+<style scoped></style>
