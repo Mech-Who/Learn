095ef8ca0fdf709ab66487123d156a5493f3ecb8
hushuhan <873933169@qq.com>
2024-03-13 22:33:28 +0800

    自定义事件验证

--- VueCode/chapter07/01_learn_component/src/04_子组件传递父组件/CounterOperation.vue
+++ VueCode/chapter07/01_learn_component/src/04_子组件传递父组件/CounterOperation.vue
@@ -9,7 +9,20 @@
 
 <script>
 export default {
-  emit: ["add", "sub"], // 1. 定义该组件可以向其父组件出发的add和sub事件
+    // 1. 数组写法
+    // emit: ["add", "sub", "addN"], // 1. 定义该组件可以向其父组件出发的add和sub事件
+    emit: {
+        add: null,  // 定义该组件可以向其父组件出发的add事件
+        sub: null,
+        addN: (num, name, age) => {
+            if (num > 10) {
+                // 如果num大于10，则验证通过
+                return true
+            }
+            // 如果num小于等于10，则返回false。控制台会出现参数验证不通过的警告，但不影响程序的运行
+            return false
+        }
+    },
   data() {
     return {
       num: 0,   // 存储输入的值
