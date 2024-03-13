0772c469da651eca01c11787867bfbc353fd656e
hushuhan <873933169@qq.com>
2024-03-13 23:49:49 +0800

    Provide和Inject基本使用

---
+++ VueCode/chapter07/01_learn_component/src/06_Provide和Inject使用/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app">
+        App
+        <home></home>
+    </div>
+</template>
+
+<script>
+    import Home from './Home.vue'
+    export default {
+        components: {
+            Home
+        },
+        // 在根组件中通过provide属性为子组件和孙子组件提供数据
+        provide: {
+            name: "why",
+            age: 18,
+            friends: ["jack", "rose"]
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ VueCode/chapter07/01_learn_component/src/06_Provide和Inject使用/HomeContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home-content">
+    HomeContent
+    <p>{{ name }} - {{ age }} - {{ friends }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  inject: ["name", "age", "friends"], // 孙子组件使用inject属性注入祖父组件（App.vue）提供的数据
+};
+</script>
+
+<style scoped></style>
