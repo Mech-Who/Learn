7ac696e899004468158d03ea813f7b62a30715f5
hushuhan <873933169@qq.com>
2024-03-13 21:33:43 +0800

    手动绑定非props的属性

--- VueCode/chapter07/01_learn_component/src/03_父组件传递子组件/NoPropAttribute.vue
+++ VueCode/chapter07/01_learn_component/src/03_父组件传递子组件/NoPropAttribute.vue
@@ -1,14 +1,11 @@
 <template>
-    <div class="no-prop-attribute">
-        该子组件没有定义任何的props属性
-        <h4 :class="$useAttrs.class">{{ $attrs.id }} - {{ $attrs.name }}</h4>
-    </div>
+    <div class="no-prop-attribute">该子组件没有定义任何的props属性(节点1)</div>
+    <div class="no-prop-attribute">该子组件没有定义任何的props属性(节点2)</div>
+    <div class="no-prop-attribute" :class="$attrs.class">该子组件没有定义任何的props属性(节点2)</div>
 </template>
 
 <script>
-    export default {
-        inheritAttrs: false
-    }
+    export default {}
 </script>
 
 <style scoped>
