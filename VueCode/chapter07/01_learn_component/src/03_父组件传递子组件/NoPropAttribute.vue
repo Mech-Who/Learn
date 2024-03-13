3a1c5115be69e18df775dc2abd633878a474880a
hushuhan <873933169@qq.com>
2024-03-13 21:10:09 +0800

    禁用非props的属性继承

--- VueCode/chapter07/01_learn_component/src/03_父组件传递子组件/NoPropAttribute.vue
+++ VueCode/chapter07/01_learn_component/src/03_父组件传递子组件/NoPropAttribute.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="no-prop-attribute">
         该子组件没有定义任何的props属性
+        <h4 :class="$useAttrs.class">{{ $attrs.id }} - {{ $attrs.name }}</h4>
     </div>
 </template>
 
 <script>
-    export default {} // 没有定义任何props
+    export default {
+        inheritAttrs: false
+    }
 </script>
 
 <style scoped>
