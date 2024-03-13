4d6563aaf9552216ac601d5f00caae8479eb75ee
hushuhan <873933169@qq.com>
2024-03-13 21:07:28 +0800

    传入非props属性

--- VueCode/chapter07/01_learn_component/src/03_父组件传递子组件/App.vue
+++ VueCode/chapter07/01_learn_component/src/03_父组件传递子组件/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div class="app">
-    <!-- 5. 直接传递字符串 -->
-    <show-message messageInfo="我是messageInfo字符串"></show-message>
-    <show-message message-info="我是message-info字符串"></show-message>
-  </div>
+    <div class="app">
+        <!-- 6. 传递非props的属性：id，class和name -->
+        <no-prop-attribute id="coder" class="why" name="coderwhy"></no-prop-attribute>
+    </div>
 </template>
 
 <script>
-import ShowMessage from './ShowMessage.vue'
-export default {
-    components: {
-        ShowMessage
-    }
-},
-data() {
-    return {
-        title: "我是标题title",
-        content: "我是内容content",
-        message: {
-            title: "我是标题message.title",
-            content: "我是内容message.content"
+    import NoPropAttribute from './NoPropAttribute.vue'
+    export default {
+        components: {
+            NoPropAttribute
         }
     }
-}
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
---
+++ VueCode/chapter07/01_learn_component/src/03_父组件传递子组件/NoPropAttribute.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="no-prop-attribute">
+        该子组件没有定义任何的props属性
+    </div>
+</template>
+
+<script>
+    export default {} // 没有定义任何props
+</script>
+
+<style scoped>
+</style>
