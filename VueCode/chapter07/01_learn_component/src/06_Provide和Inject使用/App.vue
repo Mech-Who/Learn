25b9bf8220024cce529197a58ada660ae32f358d
hushuhan <873933169@qq.com>
2024-03-14 23:24:58 +0800

    provide中如果需要使用this，这样写会报错

--- VueCode/chapter07/01_learn_component/src/06_Provide和Inject使用/App.vue
+++ VueCode/chapter07/01_learn_component/src/06_Provide和Inject使用/App.vue
@@ -6,19 +6,23 @@
 </template>
 
 <script>
-  import Home from './Home.vue'
-  export default {
-    components: {
-      Home
-    },
-    // 在根组件中通过provide属性为子组件和孙子组件提供数据
-    provide: {
-      name: "why",
-      age: 18,
-      friends: ["jack", "rose"]
-    }
-  }
+import Home from "./Home.vue";
+export default {
+  components: {
+    Home,
+  },
+  // 在根组件中通过provide属性为子组件和孙子组件提供数据
+  provide: {
+    name: "why",
+    age: 18,
+    friends: this.friends,
+  },
+  data() {
+    return {
+      friends: ["jack", "rose"],
+    };
+  },
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
