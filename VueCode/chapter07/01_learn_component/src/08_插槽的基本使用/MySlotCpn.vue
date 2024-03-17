910522faac0424b38d93b5f69b942b63ea26a4af
hushuhan <873933169@qq.com>
2024-03-17 16:12:20 +0800

    插槽的默认内容

--- VueCode/chapter07/01_learn_component/src/08_插槽的基本使用/MySlotCpn.vue
+++ VueCode/chapter07/01_learn_component/src/08_插槽的基本使用/MySlotCpn.vue
@@ -1,16 +1,15 @@
 <template>
-    <div class="my-slot-cpn">
-        <div class="header">header</div>
-        <slot name="default"></slot>
-        <!-- 可以简写为 -->
-        <!-- <slot></slot> -->
-        <div class="footer">footer</div>
-    </div>
+  <div class="my-slot-cpn">
+    <div class="header">header</div>
+    <slot name="default"> 插槽的默认内容，只在没有提供插入的内容时，才会显示 </slot>
+    <!-- 可以简写为 -->
+    <!-- <slot></slot> -->
+    <div class="footer">footer</div>
+  </div>
 </template>
 
 <script>
-    export default {}
+export default {};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
