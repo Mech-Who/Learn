5ed56120775e738f27c11cdcd55ea9e384e9ff3d
hushuhan <873933169@qq.com>
2024-03-18 21:13:31 +0800

    作用域插槽

--- VueCode/chapter07/01_learn_component/src/10_作用域插槽作用/App.vue
+++ VueCode/chapter07/01_learn_component/src/10_作用域插槽作用/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="app">
-    <child-cpn>
-      <button>{{title}}</button>
-    </child-cpn>
+    <show-names :names="names">
+      <!-- v-slot:default后面的值slotProps是插槽属性的集合。其中,slotProps可任意命名 -->
+      <template v-slot:default="slotProps">
+        <span>{{slotProps.item}}-{{slotProps.index}}</span>
+      </template>
+    </show-names>
   </div>
 </template>
 
 <script>
-  import ChildCpn from './ChildCpn.vue';
-  export default {
-    components: {
-      ChildCpn
-    }
-  }
+import ShowNames from "./ShowNames.vue"
+export default {
+  components: {
+    ShowNames
+  },
+  data() {
+    return {
+      names: ["coder", "why", "colin"],
+    };
+  },
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ VueCode/chapter07/01_learn_component/src/10_作用域插槽作用/ShowNames.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="show-names">
+    <template v-for="(item, index) in names" :key="item">
+      <!-- 为插槽添加itemm和index属性，并绑定item和index数据。item和index称为slot prop -->
+      <slot :item="item" :index="index"></slot>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: Array,
+      default: () => []
+    }
+  },
+};
+</script>
+
+<style scoped></style>
