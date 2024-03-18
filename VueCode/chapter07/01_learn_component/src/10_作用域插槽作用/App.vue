134f3caf5a95bafc50724511fba460738033cdaa
hushuhan <873933169@qq.com>
2024-03-18 20:49:12 +0800

    渲染作用域

---
+++ VueCode/chapter07/01_learn_component/src/10_作用域插槽作用/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <child-cpn>
+      <button>{{title}}</button>
+    </child-cpn>
+  </div>
+</template>
+
+<script>
+  import ChildCpn from './ChildCpn.vue';
+  export default {
+    components: {
+      ChildCpn
+    }
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ VueCode/chapter07/01_learn_component/src/10_作用域插槽作用/ChildCpn.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="child-cpn">
+    <h4>{{ title }}</h4>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "我是title",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
