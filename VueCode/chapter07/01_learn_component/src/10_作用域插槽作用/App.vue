66e09f3379b6f5d6e16baa6fe7b40da52761f02a
hushuhan <873933169@qq.com>
2024-03-18 21:54:19 +0800

    独占默认插槽

--- VueCode/chapter07/01_learn_component/src/10_作用域插槽作用/App.vue
+++ VueCode/chapter07/01_learn_component/src/10_作用域插槽作用/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="app">
-    <show-names :names="names">
-      <!-- v-slot:default后面的值slotProps是插槽属性的集合。其中,slotProps可任意命名 -->
-      <template v-slot:default="slotProps">
-        <span>{{slotProps.item}}-{{slotProps.index}}</span>
-      </template>
+    <show-names :names="names" v-slot="slotProps">
+      <!-- 独占默认插槽，v-slot:default=""可以写成v-slot="",可以省去template标签-->
+      <!-- 如果组件同时具备默认插槽和具名插槽，那么必须按照template的语法来编写 -->
+      <span>{{slotProps.item}}-{{slotProps.index}}</span>
     </show-names>
   </div>
 </template>
