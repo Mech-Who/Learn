6b0d0a5c85fcd9d74beffa5e3c90693df384d576
hushuhan <873933169@qq.com>
2024-03-29 00:18:10 +0800

    include属性使用正则表达式RegExp

--- VueCode/chapter08/01_learn_component/src/01_动态组件的使用/App.vue
+++ VueCode/chapter08/01_learn_component/src/01_动态组件的使用/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="app">
     <button
-      v-for="item in tabs" :key="item"
+      v-for="item in tabs"
+      :key="item"
       @click="itemClick(item)"
       :class="{ active: currentTab == item }"
     >
       {{ item }}
     </button>
     <!-- 2. 用keep-alive组件保留动态组件的状态，同时会保留其对应的子孙组件的状态 -->
-    <keep-alive include="home,category">
-      <component 
+    <keep-alive :include="/home|category/">
+      <component
         :is="currentTab"
         name="coderwhy"
         :age="18"
-        @pageClick="pageClick">
+        @pageClick="pageClick"
+      >
       </component>
     </keep-alive>
   </div>
