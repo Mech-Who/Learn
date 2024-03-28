ce71904e0bf0fe4fbcc93bc6855efede87835919
hushuhan <873933169@qq.com>
2024-03-29 00:21:22 +0800

    缓存实例的生命周期函数

--- VueCode/chapter08/01_learn_component/src/01_动态组件的使用/page/About.vue
+++ VueCode/chapter08/01_learn_component/src/01_动态组件的使用/page/About.vue
@@ -10,9 +10,22 @@ export default {
   name: "about",
   data() {
     return {
-      counter: 0
-    }
+      counter: 0,
+    };
   },
+  created() {
+    console.log("about created");
+  },
+  unmounted() {
+    console.log("about unmounted");
+  },
+  // 下面是<keep-alive>缓存组件实例的两个生命周期函数
+  activated() {
+    console.log("about activated");
+  },
+  deactivated() {
+    console.log("about deactivated");
+  }
 };
 </script>
 
