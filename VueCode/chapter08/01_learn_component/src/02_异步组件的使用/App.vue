40b2ea50383d31b40c2efb5422982af1e580ba00
hushuhan <873933169@qq.com>
2024-03-30 00:11:52 +0800

    defineAsyncComponent函数使用工厂函数实现异步加载组件

--- VueCode/chapter08/01_learn_component/src/02_异步组件的使用/App.vue
+++ VueCode/chapter08/01_learn_component/src/02_异步组件的使用/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="app">App组件</div>
+  <div class="app">App组件
+
+  </div>
 </template>
 
 <script>
+// 普通方式导入的Home.vue组件，不会进行分包处理。
+import Home from "./Home.vue"
 // import { sum } from "./utils/math.js"
-import("./utils/math").then(res => {
-  console.log(res.sum(20, 30));
-});
-export default {}
+import { defineAsyncComponent } from "vue";
+// 使用defineAsyncComponent函数异步加载AsyncCategoryt.vue组件，会进行分包处理
+const AsyncCategory = defineAsyncComponent(() => {
+  import("./AsyncCategory.vue")
+})
+export default {
+  component: {
+    Home,
+    AsyncCategory
+  }
+}
 </script>
---
+++ VueCode/chapter08/01_learn_component/src/02_异步组件的使用/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    Home组件
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "home"
+  }
+</script>
+
+<style scoped>
+</style>
