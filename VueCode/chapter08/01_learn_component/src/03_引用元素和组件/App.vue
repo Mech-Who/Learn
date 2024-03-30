80e14fd9cbfe94bef387f0fcd870d00667abcd4b
hushuhan <873933169@qq.com>
2024-03-30 16:09:02 +0800

    $refs

---
+++ VueCode/chapter08/01_learn_component/src/03_引用元素和组件/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app">
+    <!-- 1. 在h4上个通过ref注册一个模板引用 -->
+    <h4 ref="title">App中的h4元素</h4>
+    <!-- 2. 在<nav-bar>上通过ref注册一个模板引用 -->
+    <nav-bar ref="navBar"></nav-bar>
+
+    <button @click="btnClick">获取h4元素对象和NavBar组件实例</button>
+  </div>
+</template>
+
+<script>
+  import NavBar from "./NavBar.vue"
+  export default {
+    components: {
+      NavBar
+    },
+    data() {
+      return {
+        names: ["coder", "why"]
+      }
+    },
+    methods: {
+      btnClick() {
+        // 通过this.$refs访问已注册的模板引用(title)
+        console.log("h4元素对象=", this.$refs.title);
+        console.log("Navbar组件实例", this.$refs.navBar); // 访问已注册的模板引用（navBar）
+        console.log("访问NavBar组件实例的message便来那个:", this.$refs.navBar.message);
+        // 调用该Navbar组件实例的sayHello方法
+        this.$refs.navBar.sayHello();
+        // $el:获取Navbar组件实例的DOM元素对象
+        console.log(this.$refs.navBar.$el);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ VueCode/chapter08/01_learn_component/src/03_引用元素和组件/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="navbar">
+    NavBar 组件
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello NavBar!"
+    }
+  },
+  methods: {
+    sayHello() {
+      console.log("sayHello: Hello NavBar");
+    }
+  }
+}
+</script>
+
+<style scoped></style>
