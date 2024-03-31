8071a83bcf536b6b14adc827c4599c8ebbdd9b36
hushuhan <873933169@qq.com>
2024-03-31 23:29:47 +0800

    组件上应用多个v-model

--- VueCode/chapter08/01_learn_component/src/05_组件的v-model/App.vue
+++ VueCode/chapter08/01_learn_component/src/05_组件的v-model/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div class="app">
     <h4>App组件message变量：{{ message }}</h4>
+    <h4>App组件title变量：{{ title }}</h4>
     <button @click="changeMessage">App组件修改message</button>
     <!-- 1. 在组件中使用v-model指令 -->
     <hy-input v-model="message"></hy-input>
     <!-- 上面的v-model指令，等价于下面的语法 -->
     <!-- <hy-input :modelValue="message" @update:model-value="message = $event"></hy-input> -->
+    <!-- 2. 在组件上绑定两个v-model指令 -->
+    <hy-multiple-input v-model="message" v-model:title="title"></hy-multiple-input>
   </div>
 </template>
 
 <script>
 import HyInput from "./HyInput.vue"
+import HyMultipleInput from "./HyMultipleInput.vue"
 export default {
   components: {
-    HyInput
+    HyInput,
+    HyMultipleInput
   },
   data() {
     return {
-      message: "Hello World"
+      message: "Hello",
+      title: "World"
     }
   },
   methods: {
---
+++ VueCode/chapter08/01_learn_component/src/05_组件的v-model/HyMultipleInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hy-multiple-input">
+    HyMultipleInput组件：
+    <input :value="modelValue" @input="input1Change">
+    <input :value="title" @input="input2Change">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: String, // 1. 默认v-model绑定的属性
+    title: String,      // 2. 默认v-model绑定的属性
+  },
+  emits: ["update:modelValue", "update:title"],
+  methods: {
+    input1Change(event) {
+      this.$emit("update:modelValue", event.target.value) // 3. 默认触发的事件
+    },
+    input2Change(event) {
+      this.$emit("update:title", event.target.value)      // 4. 指定触发的事件
+    },
+  }
+}
+</script>
+
+<style scoped></style>
