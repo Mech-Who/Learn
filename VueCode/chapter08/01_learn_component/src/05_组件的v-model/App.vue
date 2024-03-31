21cfea9e84dbbe97fdbb6daa0162a034dd8d481d
hushuhan <873933169@qq.com>
2024-03-31 23:36:05 +0800

    v-model绑定对象类型

--- VueCode/chapter08/01_learn_component/src/05_组件的v-model/App.vue
+++ VueCode/chapter08/01_learn_component/src/05_组件的v-model/App.vue
@@ -1,36 +1,25 @@
 <template>
   <div class="app">
-    <h4>App组件message变量：{{ message }}</h4>
-    <h4>App组件title变量：{{ title }}</h4>
-    <button @click="changeMessage">App组件修改message</button>
-    <!-- 1. 在组件中使用v-model指令 -->
-    <hy-input v-model="message"></hy-input>
-    <!-- 上面的v-model指令，等价于下面的语法 -->
-    <!-- <hy-input :modelValue="message" @update:model-value="message = $event"></hy-input> -->
-    <!-- 2. 在组件上绑定两个v-model指令 -->
-    <hy-multiple-input v-model="message" v-model:title="title"></hy-multiple-input>
+    <h4>App组件formData.name: {{ formData.name }}</h4>
+    <h4>App组件formData.title: {{ formData.title }}</h4>
+    <hy-form v-model="formData"></hy-form>
   </div>
 </template>
 
 <script>
-import HyInput from "./HyInput.vue"
-import HyMultipleInput from "./HyMultipleInput.vue"
+import HyForm from "./HyForm.vue"
 export default {
   components: {
-    HyInput,
-    HyMultipleInput
+    HyForm
   },
   data() {
     return {
-      message: "Hello",
-      title: "World"
+      formData: {
+        name: "coder",
+        title: "why",
+      }
     }
   },
-  methods: {
-    changeMessage() {
-      this.message = "coderwhy"
-    }
-  }
 }
 </script>
 
---
+++ VueCode/chapter08/01_learn_component/src/05_组件的v-model/HyForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hy-form">
+    HyMultipleInput组件：
+    <input :value="modelValue.name" @input="inputChange($event, 'name')">
+    <input :value="modelValue.title" @input="inputChange($event, 'title')">
+    <!-- 不建议使用下面的简写。1. 会直接修改props属性。 2. 可读性也不是很好 -->
+    <!-- <input v-model="modelValue.title"> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: Object, // 为对象类型
+  },
+  emits: ["update:modelValue"],
+  methods: {
+    inputChange(event, field) {
+      this.$emit("update:modelValue", {
+        ...this.modelValue,
+        [field]: event.target.value,  // 将当前获取的值赋给对应的name和title属性
+      })
+    },
+  }
+}
+</script>
+
+<style scoped></style>
