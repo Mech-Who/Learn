d9d190a3e5a9b47af267dfefba5b78c94050ce2b
hushuhan <873933169@qq.com>
2024-03-31 23:24:27 +0800

    v-model绑定computed

--- VueCode/chapter08/01_learn_component/src/05_组件的v-model/HyInput.vue
+++ VueCode/chapter08/01_learn_component/src/05_组件的v-model/HyInput.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="hy-input">
     HyInput组件：
-    <input :value="modelValue" @input="inputClick">
+    <!-- 1. 支持 -->
+    <!-- <input :value="modelValue" @input="inputClick"> -->
+    <!-- 2. 不支持 -->
+    <!-- <input v-model="modelValue"> -->
+    <input v-model="value">
   </div>
 </template>
 
@@ -11,6 +15,16 @@ export default {
     modelValue: String
   },
   emits: ["update:modelValue"],
+  computed: {
+    value: {
+      set(value) {
+        this.$emit("update:modelValue", value); // setter方法
+      },
+      get() {
+        return this.modelValue; // getter方法
+      }
+    }
+  },
   methods: {
     inputClick(event) {
       this.$emit("update:modelValue", event.target.value)
