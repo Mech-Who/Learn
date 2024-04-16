876d22f9eda0aa4b90609c75597868fcd889686b
hushuhan <873933169@qq.com>
2024-04-16 23:02:59 +0800

    ref的浅层解包

--- VueCode/chapter10/01_composition_api/src/04_数据响应式API/App.vue
+++ VueCode/chapter10/01_composition_api/src/04_数据响应式API/App.vue
@@ -3,19 +3,22 @@
     App组件
     <!-- 每一个大写都要用-分割，所以不如写大写形式好看 -->
     <!-- <reactive-a-p-i></reactive-a-p-i> -->
-    <ReactiveAPI></ReactiveAPI>
+    <!-- <ReactiveAPI></ReactiveAPI> -->
     <!-- <ref-a-p-i></ref-a-p-i> -->
-    <RefAPI></RefAPI>
+    <!-- <RefAPI></RefAPI> -->
+    <RefAPIOther></RefAPIOther>
   </div>
 </template>
 
 <script>
 import ReactiveAPI from './ReactiveAPI.vue';
 import RefAPI from './RefAPI.vue';
+import RefAPIOther from './RefAPIOther.vue';
 export default {
   components: {
     ReactiveAPI,
-    RefAPI
+    RefAPI,
+    RefAPIOther
   }
 }
 </script>
---
+++ VueCode/chapter10/01_composition_api/src/04_数据响应式API/RefAPIOther.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="ref-api-other" style="border: 1px solid #ddd; margin: 4px">
+    RefAPIOther组件（演示ref的浅层解包）
+    <h4>当前计数：{{ counter }}</h4>
+    <!-- 4. info中的ref对象可以自动解包 -->
+    <h4>当前计数：{{ info.counter }}</h4>
+    <!-- 5. reactiveInfo中的ref对象可以自动解包 -->
+    <h4>当前计数：{{ reactiveInfo.counter }}</h4>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue'
+export default {
+  setup() {
+    let counter = ref(100); // 1. 定义响应式数据
+    const info = {  // 2. 普通对象包含ref响应式对象
+      counter
+    }
+    const reactiveInfo = reactive({ // 3. reactive响应式对象包含ref响应式对象
+      counter
+    })
+    const increment = () => {
+      counter.value++
+      console.log(counter.value);
+    }
+    return {
+      counter, increment,
+      info, reactiveInfo
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
