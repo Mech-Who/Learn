c6fd9201649f6d703009cd0176974512c03933b2
hushuhan <873933169@qq.com>
2024-04-16 22:03:45 +0800

    Mixin的基本使用

---
+++ VueCode/chapter10/01_composition_api/src/01_Mixin/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>{{ message }}</h2>
+    <button @click="foo">单击调用demoMixin定义的foo方法</button>
+  </div>
+</template>
+
+<script>
+import { demoMixin } from './mixins/demoMixin'
+export default {
+  // 将demoMixin定义的data、methods和created选项混入该组件中，也支持混入其他组件中
+  mixins: [demoMixin]
+}
+</script>
+
+<style scoped></style>
