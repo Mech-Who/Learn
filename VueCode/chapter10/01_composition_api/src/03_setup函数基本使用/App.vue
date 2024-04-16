465de8dcdeb06228866373a47bd06f60217a95df
hushuhan <873933169@qq.com>
2024-04-16 22:32:03 +0800

    setup函数的第二个参数context

--- VueCode/chapter10/01_composition_api/src/03_setup函数基本使用/App.vue
+++ VueCode/chapter10/01_composition_api/src/03_setup函数基本使用/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="app" style="border: 1px solid #ddd;margin: 4px">
-    <setup-props message="学习setup函数的参数"></setup-props>
+    App组件
+    <!-- <setup-props message="学习setup函数的参数"></setup-props> -->
+    <setup-context message="学习setup context参数" id="code" class="why"></setup-context>
   </div>
 </template>
 
 <script>
-import SetupProps from './SetupProps.vue'
+import SetupProps from './SetupProps.vue';
+import SetupContext from './SetupContext.vue';
 export default {
   components: {
-    SetupProps
+    SetupProps,
+    SetupContext
   }
 }
 </script>
---
+++ VueCode/chapter10/01_composition_api/src/03_setup函数基本使用/SetupContext.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="setup-context" style="border: 1px solid #ddd;margin:10px">
+    App组件
+    <h4>{{ message }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props, context) {         // context对象参数支持解构
+    console.log(props.message);   // 1. 所有props属性
+    console.log(context.attrs.id, context.attrs.class); // 2. 所有非props属性
+    console.log(context.slots);   // 3. 父组件传递过来的插槽
+    console.log(context.emit);    // 4. 用于触发事件，如context.emit("name", value)
+  }
+}
+</script>
+
+<style scoped>
+</style>
