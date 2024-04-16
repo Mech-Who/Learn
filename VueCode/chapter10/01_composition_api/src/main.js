1da2df00d1927d538cbbeac6268b9b62f8acea6d
hushuhan <873933169@qq.com>
2024-04-16 22:12:24 +0800

    全局混入Mixin

--- VueCode/chapter10/01_composition_api/src/main.js
+++ VueCode/chapter10/01_composition_api/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './01_Mixin/App.vue'
 
-createApp(App).mount('#app')
+let app = createApp(App)
+
+// 1. 使用app.mixin方法，全局混入Mixin对象
+app.mixin({
+  created() {
+    console.log("global mixin created");
+  }
+})
+
+app.mount('#app')
