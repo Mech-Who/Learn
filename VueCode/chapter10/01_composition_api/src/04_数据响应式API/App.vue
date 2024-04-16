f1eaf7463fbb66e8677572bc704af2802efaeeee
hushuhan <873933169@qq.com>
2024-04-16 22:57:29 +0800

    添加说明

--- VueCode/chapter10/01_composition_api/src/04_数据响应式API/App.vue
+++ VueCode/chapter10/01_composition_api/src/04_数据响应式API/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="app" style="border: 1px solid #ddd; margin: 4px">
     App组件
-    <reacitve-api></reacitve-api>
-    <ref-api></ref-api>
+    <!-- 每一个大写都要用-分割，所以不如写大写形式好看 -->
+    <!-- <reactive-a-p-i></reactive-a-p-i> -->
+    <ReactiveAPI></ReactiveAPI>
+    <!-- <ref-a-p-i></ref-a-p-i> -->
+    <RefAPI></RefAPI>
   </div>
 </template>
 
