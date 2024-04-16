6d6328053eb57f586663fb07572b71517a9c9702
hushuhan <873933169@qq.com>
2024-04-16 22:37:49 +0800

    setup函数的返回值

---
+++ VueCode/chapter10/01_composition_api/src/03_setup函数基本使用/SetupReturn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="setup-return" style="border: 1px solid #ddd;margin: 4px">
+    SetupReturn组件
+    <h4>当前计数：{{ counter }}</h4>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  setup(props, context) {
+    let counter = 100;    // 1. 局部变量
+    const increment = () => { // 2. 局部函数，代替在methods中定义的方法
+      counter++;
+      console.log(counter);
+    }
+    // 3. setup函数的返回值，返回值中的属性可直接在模板中使用
+    return {
+      counter: counter,     // 4. 返回一个内部变量，供模板使用
+      increment: increment  // 5. 返回一个方法/函数，供模板使用
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
