6c7e10f88a2c548c2915661ab95e7086d8c6356a
hushuhan <873933169@qq.com>
2024-04-19 00:14:48 +0800

    computed函数的基本使用2

--- VueCode/chapter10/01_composition_api/src/07_computed函数的基本使用/ComputedAPI.vue
+++ VueCode/chapter10/01_composition_api/src/07_computed函数的基本使用/ComputedAPI.vue
@@ -13,10 +13,21 @@ export default {
     const firstName = ref("Kobe");
     const lastName = ref("Bryant");
     // 1. 方式一： 传入一个getter函数。computed返回值是一个ref对象
-    const fullName = computed(() => firstName.value + " " + lastName.value);
+    // const fullName = computed(() => firstName.value + " " + lastName.value);
+    // 2. 方式二： 传入一个对象，对象包含get和set方法
+    const fullName = computed({
+      get: () => firstName.value + " " + lastName.value,  // 2. getter方法
+      set(newValue) { // 3. setter方法
+        const names = newValue.split(" ");
+        firstName.value = names[0];
+        lastName.value = names[1];
+      }
+    })
     const changeName = () => {
       // 3. 修改firstName，计算属性会重新计算
-      firstName.value = "James"
+      // firstName.value = "James"
+      // 4. 修改fullName计算属性
+      fullName.value = "James Bryant";
     }
     return {
       fullName,
