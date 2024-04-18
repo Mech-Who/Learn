02ee923e6f37c2200ace6959461694b67173a10d
hushuhan <873933169@qq.com>
2024-04-18 23:42:15 +0800

    ref其他知识点3

--- VueCode/chapter10/01_composition_api/src/06_ref其他知识点/ToRefsAPI.vue
+++ VueCode/chapter10/01_composition_api/src/06_ref其他知识点/ToRefsAPI.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue';
+import { reactive, toRef } from 'vue';
 export default {
   setup() {
     const info = reactive({
@@ -15,8 +15,11 @@ export default {
     });
     // 1. ES6语法直接结构info对象，会失去响应式
     // let { name, age } = info;
-    // toRefs 函数将reactive返回对象中的所有属性都转换成ref对象
-    let { name, age } = toRefs(info);
+    // 2. toRefs 函数将reactive返回对象中的所有属性都转换成ref对象
+    // let { name, age } = toRefs(info);
+    // 3. toRef函数将某个属性转换为ref对象
+    let name = toRef(info, "name"); // 仅将name属性转换为ref对象
+    let age = toRef(info, "age"); // 仅将age属性转换为ref对象
     const changeAge = () => {
       info.age++; // 或 age.value++，任何一个修改都会引起另一个变化
       console.log(info.age);
