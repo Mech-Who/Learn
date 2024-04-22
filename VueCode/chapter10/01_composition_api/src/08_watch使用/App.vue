6e70143988ca44feb6634cdd897fc3da554800ca
hushuhan <873933169@qq.com>
2024-04-22 21:03:00 +0800

    补充App.vue的内容

--- VueCode/chapter10/01_composition_api/src/08_watch使用/App.vue
+++ VueCode/chapter10/01_composition_api/src/08_watch使用/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="app">
     App组件
-    <WatchEffectAPI></WatchEffectAPI>
+    <!-- <WatchEffectAPI></WatchEffectAPI> -->
+    <WatchAPI></WatchAPI>
   </div>
 </template>
 
 <script>
 import WatchEffectAPI from './WatchEffectAPI.vue';
+import WatchAPI from './WatchAPI.vue'
 export default {
   components: {
-    WatchEffectAPI
+    WatchEffectAPI,
+    WatchAPI
   }
 }
 </script>
