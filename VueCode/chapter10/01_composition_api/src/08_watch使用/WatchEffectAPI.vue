8a0b0ba185947c839e83b38b571ee2b06fc94b21
hushuhan <873933169@qq.com>
2024-04-22 20:15:54 +0800

    watchEffect清除副作用

--- VueCode/chapter10/01_composition_api/src/08_watch使用/WatchEffectAPI.vue
+++ VueCode/chapter10/01_composition_api/src/08_watch使用/WatchEffectAPI.vue
@@ -12,15 +12,18 @@ export default {
     const age = ref(18);
     // 1. watchEffect 会自动收集响应式依赖，默认先执行一次，但是获取不到新值和旧值
     // 3. stop是watchEffect函数返回的函数，专门用于停止监听
-    const stop = watchEffect(() => {
+    const stop = watchEffect((onInvalidate) => {
+      const timer = setTimeout(() => {
+        console.log("模拟网络请求，网络请求成功~");
+      }, 2000)
+      onInvalidate(() => {
+        // 1. 监听到agee变化或监听停止时，会执行这里的代码
+        clearTimeout(timer);  // 2. 清除上一次的定时器
+        console.log("onInvalidate");
+      })
       console.log("age:", age.value); // 2. 监听age的变化，age变化后会再次执行该回调函数
     });
-    const changeAge = () => {
-      age.value++
-      if (age.value > 20) {
-        stop(); // 4. 停止监听age的变化
-      }
-    }
+    const changeAge = () => age.value++
     return {
       age,
       changeAge
