30f726a6ceeac861ce166f4342d8c647ab4697e3
hushuhan <873933169@qq.com>
2024-04-22 20:56:09 +0800

    watchEffect的执行时机

---
+++ VueCode/chapter10/01_composition_api/src/08_watch使用/WatchEffectAPIFlush.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="watch-effect-api-flush">
+    <!-- 5. h4元素挂载玩成后，会自动赋值到titleRef内部值中 -->
+    <h4 ref="titleRef">哈哈哈</h4>
+  </div>
+</template>
+
+<script>
+import { watchEffect } from 'vue';
+
+export default {
+  setup() {
+    // 1. 定义一个titleRef对象，用于存储h4元素的DOM对象
+    const titleRef = ref(null);
+    // 2. 监听titleeRef的变化，即赋值操作
+    watchEffect(() => {
+      console.log(titleRef.value);  // 3. 打印h4元素的DOM对象
+    })
+    return { titleRef } // 4. 返回titleeRef对象给template使用
+  }
+}
+</script>
+
+<style scoped>
+</style>
