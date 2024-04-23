2860a07976b44afbb90100945cf2d2aa0003f46c
hushuhan <873933169@qq.com>
2024-04-24 00:38:21 +0800

    defineProps和defineEmits

--- VueCode/chapter10/01_composition_api/src/12_script_setup/App.vue
+++ VueCode/chapter10/01_composition_api/src/12_script_setup/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="app">
     App组件
+    <DefinePropsEmitAPI message="App传递过来的message" @increment="getCounter"></DefinePropsEmitAPI>
     <ScriptSetupExample></ScriptSetupExample>
   </div>
 </template>
 
-<script>
+<script setup>
 import ScriptSetupExample from './ScriptSetupExample.vue';
-export default {
-  components: {
-    ScriptSetupExample
-  }
-}
+import DefinePropsEmitAPI from './DefinePropsEmitAPI.vue';
+
+const getCounter = (number) => console.log("App组件拿到子组件传递过来的number：" + number);
+
 </script>
 
 <style scoped></style>
