9a75af348838b0041464f461e67ce0361de835b8
hushuhan <873933169@qq.com>
2024-04-24 00:44:16 +0800

    defineExpose

--- VueCode/chapter10/01_composition_api/src/12_script_setup/App.vue
+++ VueCode/chapter10/01_composition_api/src/12_script_setup/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app">
     App组件
+    <DefineExposeAPI ref="defineExposeAPI"></DefineExposeAPI>
     <DefinePropsEmitAPI message="App传递过来的message" @increment="getCounter"></DefinePropsEmitAPI>
     <ScriptSetupExample></ScriptSetupExample>
   </div>
@@ -9,6 +10,20 @@
 <script setup>
 import ScriptSetupExample from './ScriptSetupExample.vue';
 import DefinePropsEmitAPI from './DefinePropsEmitAPI.vue';
+import DefineExposeAPI from './DefineExposeAPI.vue';
+
+import { ref, watchEffect } from 'vue'
+
+// 1. 获取DefineExposeAPI.vue组件的实例和该组件暴露的属性
+const defineExposeAPI = ref(null)
+watchEffect(() => {
+  console.log(defineExposeAPI.value); // 1.1 组件的实例
+  console.log(defineExposeAPI.value.name); // 1.2 响应式数据
+  console.log(defineExposeAPI.value.age);
+  defineExposeAPI.value.showMessage()
+}, {
+  flush: "post"
+})
 
 const getCounter = (number) => console.log("App组件拿到子组件传递过来的number：" + number);
 
---
+++ VueCode/chapter10/01_composition_api/src/12_script_setup/DefineExposeAPI.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="define-expose-api" style="border: 1px solid #ddd; margin: 4px">
+    DefineExposeAPI组件
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const age = 18; // 普通数据
+const name = ref('coderwhy'); // 响应式数据
+const showMessage = () => console.log("showMessage方法"); // 定义方法
+// 1. 该组件需要暴露出去的属性和方法
+defineExpose({ age, name, showMessage })
+</script>
+
+<style scoped></style>
