85d5eb721e374e9b5e01a2e1fb5b519085ef3d45
hushuhan <873933169@qq.com>
2024-04-29 00:20:58 +0800

    render函数实现计数器

--- VueCode/chapter11/01_learn_component/src/01_render函数的使用/App.vue
+++ VueCode/chapter11/01_learn_component/src/01_render函数的使用/App.vue
@@ -2,9 +2,11 @@
   <div class="app" style="border: 1px solid #ddd; margin: 4px">
     App组件
     <RenderExample></RenderExample>
+    <RenderCounter></RenderCounter>
   </div>
 </template>
 
 <script setup>
 import RenderExample from './RenderExample.vue';
+import RenderCounter from './RenderCounter.vue';
 </script>
---
+++ VueCode/chapter11/01_learn_component/src/01_render函数的使用/RenderCounter.vue
@@ -0,0 +1,20 @@
+<script>
+// 1. 导入h函数
+import { h } from 'vue';
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  render() {
+    // 2. 创建布局：div标签中包含一个h4元素和两个button元素
+    return h("div", {class: "render-counter"}, [
+      // 参数1：标签名称。参数2：标签属性。参数3：标签内容。
+      h("h4", null, `当前计数： ${this.counter}`),
+      h("button", {onClick: () => this.counter++}, "+1"),
+      h("button", {onClick: () => this.counter--}, "-1"),
+    ])
+  }
+}
+</script>
