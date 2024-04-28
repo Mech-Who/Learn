8324c0c7f2f36247fd935165dacd0040f3b84fbd
hushuhan <873933169@qq.com>
2024-04-29 00:16:32 +0800

    h函数的基本使用

---
+++ VueCode/chapter11/01_learn_component/src/01_render函数的使用/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="app" style="border: 1px solid #ddd; margin: 4px">
+    App组件
+    <RenderExample></RenderExample>
+  </div>
+</template>
+
+<script setup>
+import RenderExample from './RenderExample.vue';
+</script>
---
+++ VueCode/chapter11/01_learn_component/src/01_render函数的使用/RenderExample.vue
@@ -0,0 +1,22 @@
+<script>
+import { h } from 'vue';
+
+export default {
+  // 2. 在render函数中使用h函数
+  render() {
+    // 下面等价于创建：<h4 class="title">RenderExample组件</h4> 元素
+    return h("h4", {class: "title"}, "RenderExample组件")
+  },
+  // 等价写法：
+  // 1. 在setup函数中使用h函数，return需要返回一个函数
+  setup() {
+    return () => h("h4", {class: "title"}, "RenderExample组件")
+  }
+}
+</script>
+
+<style scoped>
+.title{
+  text-decoration: underline;
+}
+</style>
