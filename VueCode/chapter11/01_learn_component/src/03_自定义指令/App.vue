8edf8a5aecffe84f34571c497ff02b921306cd44
hushuhan <873933169@qq.com>
2024-04-30 20:16:19 +0800

    自定义一个局部指令v-focus

--- VueCode/chapter11/01_learn_component/src/03_自定义指令/App.vue
+++ VueCode/chapter11/01_learn_component/src/03_自定义指令/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="app" style="border: 1px solid #ddd; margin: 4px">
     App组件
-    <DefaultImp></DefaultImp>
+    <!-- <DefaultImp></DefaultImp> -->
+    <CustomLocalDirective></CustomLocalDirective>
   </div>
 </template>
 
 <script setup>
-import DefaultImp from './DefaultImp.vue'
+import DefaultImp from './DefaultImp.vue';
+import CustomLocalDirective from './CustomLocalDirective.vue';
 </script>
 
 <style scoped></style>
---
+++ VueCode/chapter11/01_learn_component/src/03_自定义指令/CustomLocalDirective.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="custom-local-directive" style="border: 1px solid #ddd; margin: 4px">
+    <!-- 4. 使用v-focus指令 -->
+    <input type="text" v-focus>
+  </div>
+</template>
+
+<script>
+export default {
+  directives: { // 1. 自定义v-focus局部指令
+    focus: {
+      mounted(el, bindings) { // 2. 自定义指令的生命周期mounted
+        console.log("focus mounted");
+        el.focus(); // 3. el是input元素对象
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
