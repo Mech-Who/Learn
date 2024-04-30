b4090957d867c778f77dfc7db411f4ac7a19a662
hushuhan <873933169@qq.com>
2024-04-30 22:20:57 +0800

    第二种方式，在生命周期函数onUpdated中添加回调函数，每次更新都执行回调函数，可以获取到正确的h4元素的高度。

--- VueCode/chapter11/01_learn_component/src/06_nexttick的使用/App.vue
+++ VueCode/chapter11/01_learn_component/src/06_nexttick的使用/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="app" style="border: 1px solid #ddd; margin: 4px">
     App组件
-    <NextickExample1></NextickExample1>
+    <!-- <NextickExample1></NextickExample1> -->
+    <NextickExample2></NextickExample2>
   </div>
 </template>
 
 <script>
-import NextickExample1 from './NextickExample1.vue';
+// import NextickExample1 from './NextickExample1.vue';
+import NextickExample2 from './NextickExample2.vue';
 export default {
   components: {
-    NextickExample1
+    // NextickExample1
+    NextickExample2
   }
 }
 </script>
---
+++ VueCode/chapter11/01_learn_component/src/06_nexttick的使用/NextickExample2.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="next-tick-example-1" style="border: 1px solid #ddd; margin: 4px">
+    <div>计数器：{{ counter }}</div>
+    <button @click="increment">+1</button>
+    <h4 style="width: 80px" ref="titleRef">{{ message }}</h4>
+    <button @click="addMessageContent">添加内容</button>
+  </div>
+</template>
+
+<script>
+import { onUpdated, ref } from 'vue'
+export default {
+  setup() {
+    const message = ref('')
+    const titleRef = ref(null)
+    const counter = ref(0)
+    const addMessageContent = () => {
+      message.value += "更新DOM"  // 2. 触发更新h4元素内容
+    }
+    const increment = () => {counter.value ++;} // 3. 触发更新div元素内容
+    // 1. onUpdated生命周期函数。凡是DOM更新，都会被回调
+    onUpdated(() => console.log("获取到h4元素的高度：", titleRef.value.offsetHeight))
+    return {message, titleRef, counter, addMessageContent, increment}
+  }
+}
+</script>
+
+<style scoped>
+</style>
