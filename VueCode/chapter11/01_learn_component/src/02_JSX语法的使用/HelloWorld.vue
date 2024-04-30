384406504311decc09da40e6d237d694b2bc7775
hushuhan <873933169@qq.com>
2024-04-30 19:55:13 +0800

    组件的JSX语法

---
+++ VueCode/chapter11/01_learn_component/src/02_JSX语法的使用/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  render() {
+    return (
+      <div class="app" style="border: 1px solid #ddd; margin: 4px">
+        <div>HelloWorld组件</div>
+        {/**这里编写的是注释，下面定义了default插槽*/}
+        {this.$slots.default ? this.$slots.default(): <span>插槽默认内容</span>}
+      </div>
+    )
+  }
+}
+</script>
--- VueCode/chapter11/01_learn_component/src/main.js
+++ VueCode/chapter11/01_learn_component/src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './01_render函数的使用/App.vue'
+import App from './02_JSX语法的使用/App.vue'
 
 createApp(App).mount('#app')
