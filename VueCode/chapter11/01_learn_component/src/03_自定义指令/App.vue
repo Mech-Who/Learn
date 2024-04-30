94669113fd8f37d6294f9cd9273559e710c5202e
hushuhan <873933169@qq.com>
2024-04-30 20:12:30 +0800

    默认方式实现自定义组件

---
+++ VueCode/chapter11/01_learn_component/src/03_自定义指令/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app" style="border: 1px solid #ddd; margin: 4px">
+    App组件
+    <DefaultImp></DefaultImp>
+  </div>
+</template>
+
+<script setup>
+import DefaultImp from './DefaultImp.vue'
+</script>
+
+<style scoped></style>
---
+++ VueCode/chapter11/01_learn_component/src/03_自定义指令/DefaultImp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="default-imp" style="border: 1px solid #ddd; margin: 4px">
+    <input type="text" ref="input" />
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+export default {
+  setup() {
+    const input = ref(null);
+    onMounted(() => input.value.focus())  // 1. 默认方式，当挂载完后自动获取焦点
+    return { input }
+  }
+}
+</script>
+
+<style scoped>
+</style>
