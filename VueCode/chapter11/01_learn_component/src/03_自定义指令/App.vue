b06cbc6c354c42ffbc686371eac17561b945fad5
hushuhan <873933169@qq.com>
2024-04-30 20:31:40 +0800

    自定义指令的生命周期函数

--- VueCode/chapter11/01_learn_component/src/03_自定义指令/App.vue
+++ VueCode/chapter11/01_learn_component/src/03_自定义指令/App.vue
@@ -2,13 +2,15 @@
   <div class="app" style="border: 1px solid #ddd; margin: 4px">
     App组件
     <!-- <DefaultImp></DefaultImp> -->
-    <CustomLocalDirective></CustomLocalDirective>
+    <!-- <CustomLocalDirective></CustomLocalDirective> -->
+    <DirectiveLifeCycle></DirectiveLifeCycle>
   </div>
 </template>
 
 <script setup>
-import DefaultImp from './DefaultImp.vue';
-import CustomLocalDirective from './CustomLocalDirective.vue';
+// import DefaultImp from './DefaultImp.vue';
+// import CustomLocalDirective from './CustomLocalDirective.vue';
+import DirectiveLifeCycle from './DirectiveLifeCycle.vue';
 </script>
 
 <style scoped></style>
