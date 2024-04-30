b31f3005eeb3b5a3184984f2fc8be58100c2140b
hushuhan <873933169@qq.com>
2024-04-30 20:17:58 +0800

    自定义一个全局指令v-focus

--- VueCode/chapter11/01_learn_component/src/main.js
+++ VueCode/chapter11/01_learn_component/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './03_自定义指令/App.vue'
 
-createApp(App).mount('#app')
+let app = createApp(App)
+// 1. 自定义v-focus全局指令，该指令可以全局使用
+app.directive("focus", {
+  mounted(el, bindings) {
+    console.log("focus mounted");
+    el.focus();
+  }
+})
+
+app.mount('#app')
