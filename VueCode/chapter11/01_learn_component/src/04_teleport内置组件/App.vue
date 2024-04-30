aac4576302da4518a82c75c85b5a5ff48a30545a
hushuhan <873933169@qq.com>
2024-04-30 21:49:04 +0800

    teleport的基本使用

---
+++ VueCode/chapter11/01_learn_component/src/04_teleport内置组件/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app" style="border: 1px solid #ddd; margin: 4px">
+    App组件
+    <teleport to="body">
+      <h4>h4挂载到body上，而不是id=app的div上</h4>
+    </teleport>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
