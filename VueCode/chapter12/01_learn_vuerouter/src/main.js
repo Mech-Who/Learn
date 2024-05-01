2f7221be47ddc56d2d013205dfe0ecdfb07de0be
hushuhan <873933169@qq.com>
2024-05-01 20:37:33 +0800

    第五步：将路由插件安装到Vue.js 3框架中

--- VueCode/chapter12/01_learn_vuerouter/src/main.js
+++ VueCode/chapter12/01_learn_vuerouter/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './index'
+
+const app = createApp(App)
+
+// 1. 安装路由插件
+app.use(router)
+
+app.mount('#app')
