aad0ec7b2013eda3425d316e15e6419b56515a5b
hushuhan <873933169@qq.com>
2024-05-02 12:31:25 +0800

    1. 配置默认路径，只需要指定在路由配置中配置：默认路径（/）； 2. 路由可以设置history模式

--- VueCode/chapter12/01_learn_vuerouter/src/index.js
+++ VueCode/chapter12/01_learn_vuerouter/src/index.js
@@ -3,9 +3,13 @@ import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router
 // 1. 导入页面组件，也称路由组件或页面组件
 import Home from './pages/Home.vue'
 import About from './pages/About.vue'
+import { compile } from 'vue'
 
 const routes = [ // 2. 配置路由映射表（路径 -> 组件）
   {
+    path: '/',
+    redirect: '/home' // 1. 路由默认路径（/），重定向到/home路径 
+  },{
     path: '/home',
     component: Home
   },{
@@ -17,8 +21,8 @@ const routes = [ // 2. 配置路由映射表（路径 -> 组件）
 // 3. 导出创建好的路由对象
 const router = createRouter({
   routes,
-  history: createWebHashHistory() // 4. 指定用hash路由
-  // history: createWebHistory()  // 4. 指定用history路由
+  // history: createWebHashHistory() // 4. 指定用hash路由
+  history: createWebHistory()  // 4. 指定用history路由
 })
 
 export default router
