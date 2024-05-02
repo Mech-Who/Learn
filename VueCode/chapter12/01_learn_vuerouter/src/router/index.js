b892f1712331b5fecf899a71a72e4a0cb2a34586
hushuhan <873933169@qq.com>
2024-05-02 12:48:01 +0800

    路由懒加载以及分包命名

--- VueCode/chapter12/01_learn_vuerouter/src/router/index.js
+++ VueCode/chapter12/01_learn_vuerouter/src/router/index.js
@@ -1,20 +1,16 @@
 import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 
-// 1. 导入页面组件，也称路由组件或页面组件
-import Home from './pages/Home.vue'
-import About from './pages/About.vue'
-import { compile } from 'vue'
-
 const routes = [ // 2. 配置路由映射表（路径 -> 组件）
   {
     path: '/',
     redirect: '/home' // 1. 路由默认路径（/），重定向到/home路径 
   },{
     path: '/home',
-    component: Home
+    // 1. import函数的参数中添加了魔法注释webpackChunkName
+    component: () => import(/* webpackChunkName: "home-chunk" */ '../pages/Home.vue')  // 2. 路由懒加载，加载Home和About组件
   },{
     path: '/about',
-    component: About
+    component: () => import(/* webpackChunkName: "about-chunk" */ '../pages/About.vue')
   }
 ]
 
