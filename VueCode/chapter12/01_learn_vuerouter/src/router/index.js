ea75e9da32f298556809354a69a10c5cbf023db1
hushuhan <873933169@qq.com>
2024-05-02 12:54:44 +0800

    路由的其他属性name和meta

--- VueCode/chapter12/01_learn_vuerouter/src/router/index.js
+++ VueCode/chapter12/01_learn_vuerouter/src/router/index.js
@@ -3,13 +3,21 @@ import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router
 const routes = [ // 2. 配置路由映射表（路径 -> 组件）
   {
     path: '/',
-    redirect: '/home' // 1. 路由默认路径（/），重定向到/home路径 
+    redirect: '/home' // 3. 路由默认路径（/），重定向到/home路径 
   },{
     path: '/home',
-    // 1. import函数的参数中添加了魔法注释webpackChunkName
-    component: () => import(/* webpackChunkName: "home-chunk" */ '../pages/Home.vue')  // 2. 路由懒加载，加载Home和About组件
+    // 6. 指定路由名称
+    name: 'home',
+    // 4. 路由懒加载，加载Home和About组件
+    // 5. import函数的参数中添加了魔法注释webpackChunkName
+    component: () => import(/* webpackChunkName: "home-chunk" */ '../pages/Home.vue')
   },{
     path: '/about',
+    // 7. 为路由添加自定义数据
+    meta: {
+      name: 'why',
+      age: 18
+    },
     component: () => import(/* webpackChunkName: "about-chunk" */ '../pages/About.vue')
   }
 ]
