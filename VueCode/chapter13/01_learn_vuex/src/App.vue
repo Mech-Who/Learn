be04df067ee72c1f7176d219b9a7cdf9ed084425
hushuhan <873933169@qq.com>
2024-05-04 00:05:11 +0800

    使用mutation来修改state中的数据

--- VueCode/chapter13/01_learn_vuex/src/App.vue
+++ VueCode/chapter13/01_learn_vuex/src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <div>当前计数：{{ $store.state.counter }}</div>
+  <button @click="increment">+1</button>
+  <button @click="decrement">-1</button>
 </template>
 
 <script>
+import { useStore } from 'vuex';  // 2. 导入vuex提供的useStore这个Hook函数
+
 export default {
   name: 'App',
+  components: {},
+  methods: {
+    increment() {
+      this.$store.commit('increment') // 1. Options API语法触发调用mutations的方法
+    },
+  },
+  setup() {
+    const store = useStore()  // 3. setup不能用this，所以调用useStore函数获取全局store对象
+    const decrement = () => {
+      store.commit('decrement') // 4. Composition API语法触发mutations的方法
+    }
+    return { decrement }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- VueCode/chapter13/01_learn_vuex/src/store/index.js
+++ VueCode/chapter13/01_learn_vuex/src/store/index.js
@@ -1,8 +1,18 @@
 import { createStore } from 'vuex';
 const store = createStore({
-  state() { // 1. 定义全局共享的状态
+  // 1. 定义全局共享的状态
+  state() {
     return {
       counter: 0
+    }
+  },
+  // 2. 在mutations中修改全局状态
+  mutations: {
+    increment(state)  { // 3. 定义increment函数，函数state是state()函数返回的对象
+      state.counter++   // 4. 修改全局的counter
+    },
+    decrement(state) {
+      state.counter--
     }
   }
 })
