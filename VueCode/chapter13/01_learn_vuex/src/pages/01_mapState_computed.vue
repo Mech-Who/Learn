46b32c95c6c339c50b0818484d405ed993edc88a
hushuhan <873933169@qq.com>
2024-05-04 01:24:29 +0800

    mapState函数的接收数组类型参数的用法

--- VueCode/chapter13/01_learn_vuex/src/pages/01_mapState_computed.vue
+++ VueCode/chapter13/01_learn_vuex/src/pages/01_mapState_computed.vue
@@ -3,7 +3,6 @@
     <h4>Home: {{ $store.state.counter }}</h4>
     <h4>Home: {{ counter }}</h4>
     <h4>Home: {{ name }}</h4>
-    <h4>Home: {{ hyAge }}</h4>
   </div>
 </template>
 
@@ -11,20 +10,7 @@
 import { mapState } from 'vuex';
 
 export default {
-  data() {
-    return {
-      firstName: 'coder'
-    }
-  },
-  computed: mapState({  // 1. mapState函数接收对象类型参数
-    counter: (state) => { // 2. 这里的箭头函数不绑定this，会接收一个state参数，并直接返回state中的数据
-      return state.counter;
-    },
-    name(state) {  // 3. 普通函数会绑定this，这里的this是组件的实例
-      return this.firstName + state.name
-    },
-    hyAge: 'age', // 4. key可自定义，这里自定义为hyAge，会直接映射state中的age变量
-  })
+  computed: mapState(['counter', 'name']) // 3. mapState函数接收数组类型参数
 }
 </script>
 
