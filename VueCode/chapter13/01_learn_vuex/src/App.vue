13eaa996a5f63bff6f9a4a33f18663af4a014096
hushuhan <873933169@qq.com>
2024-05-03 22:41:26 +0800

    使用vuex变量

--- VueCode/chapter13/01_learn_vuex/src/App.vue
+++ VueCode/chapter13/01_learn_vuex/src/App.vue
@@ -1,16 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>当前计数：{{ $store.state.counter }}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
