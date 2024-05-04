393344f0d936d4666b1276d938490a3e5e330871
hushuhan <873933169@qq.com>
2024-05-04 14:24:42 +0800

    在computed的函数中使用mapGetters辅助函数

--- VueCode/chapter13/01_learn_vuex/src/pages/04_mapGetters_computed.vue
+++ VueCode/chapter13/01_learn_vuex/src/pages/04_mapGetters_computed.vue
@@ -2,11 +2,22 @@
   <div class="map-getters-computed">
     <h4>书籍总价：{{ $store.getters.totalPrice }}</h4>
     <h4>Vue.js 书籍总价：{{ $store.getters.totalPriceByName('Vue.js') }}</h4>
+
+    <h4>mapGetters 书籍总价: {{ totalPrice }}</h4>
+    <h4>mapGetters 书籍折扣: {{ discount }}</h4>
   </div>
 </template>
 
 <script>
-export default {}
+import { mapGetters } from 'vuex';  // 1. 导入mapGetters辅助函数
+export default {
+  computed: {
+    ...mapGetters(['totalPrice']),  // 2. mapGetters函数接收数组的语法
+    ...mapGetters({ // 3. 接收对象的语法
+      discount: 'currentDiscount'
+    })
+  }
+}
 </script>
 
 <style scoped>
