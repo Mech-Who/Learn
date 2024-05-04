9d7d8778cea23c200a03978a0a6fdf5ede87d22b
hushuhan <873933169@qq.com>
2024-05-04 14:47:21 +0800

    mutations的基本使用

---
+++ VueCode/chapter13/01_learn_vuex/src/pages/05_mutation的基本使用.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h4>当前计数：{{ $store.state.counter }}</h4>
+    <button @click="$store.commit('increment')">+1</button>
+    <button @click="$store.commit('decrement')">-1</button>
+  </div>
+</template>
