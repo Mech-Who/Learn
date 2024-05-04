296cf49bce0a3ba7cd240db40cf5c18bedc60c8d
hushuhan <873933169@qq.com>
2024-05-04 13:30:56 +0800

    mapState的setup写法

---
+++ VueCode/chapter13/01_learn_vuex/src/pages/02_mapState_setup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="mapstate-setup">
+    <h4>Setup: {{ $store.state.counter }}</h4>
+    <h4>Setup: {{ counter }}</h4>
+    <h4>Name: {{ name }}</h4>
+    <h4>Age: {{ age }}</h4>
+  </div>
+</template>
+
+<script>
+import { mapState, useStore } from 'vuex';
+import { computed } from 'vue';
+export default {
+  setup() {
+    const store = useStore();
+    // 1. 在computed中通过store读取状态
+    const counter = computed(() => store.state.counter)
+    const home = computed(() => store.state.home);
+    const hyAge = computed(() => store.state.age);
+    return { counter, home, hyAge }
+  }
+}
+</script>
+
+<style scoped></style>
