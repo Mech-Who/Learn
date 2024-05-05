7f130dc9cdba96bf2e663488ef9747ac6dd32cf0
hushuhan <873933169@qq.com>
2024-05-05 23:56:03 +0800

    actions返回Promise对象

---
+++ VueCode/chapter13/01_learn_vuex/src/pages/09_actions的返回Promise.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h4>uuid: {{ $store.state.uuid }}</h4>
+  </div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+import { useStore } from 'vuex'
+export default {
+  setup() {
+    const store = useStore()
+    onMounted(() => {
+      // 1. 分发一个getUUIDAction，并返回一个Promise对象
+      const promise = store.dispatch("getUUIDAction")
+      promise.then((res) => { // 2. then函数处理异步请求成功的结果
+        console.log(res);
+      })
+    })
+    return {}
+  }
+}
+</script>
+
+<style scoped></style>
--- VueCode/chapter13/01_learn_vuex/src/store/index.js
+++ VueCode/chapter13/01_learn_vuex/src/store/index.js
@@ -14,6 +14,7 @@ const store = createStore({
         {name: "webpack", count: 4, price: 25},
       ],
       discount: 0.9 // 书籍打9折，该变量暂时没用到，在下一个案例中会用到
+      uuid: null
     }
   },
   // 2. 在mutations中修改全局状态
@@ -27,6 +28,9 @@ const store = createStore({
     // 1. 定义类型为incrementN的mutation函数，这里的payload是对象类型
     [INCREMENT_N](state, payload){
       state.counter += payload.num  // 2. 修改counter值
+    },
+    addUUID(state, payload) {
+      state.uuid = payload
     }
   },
   // 3. 在getters中定义方法
@@ -69,6 +73,24 @@ const store = createStore({
     },
     incrementNAction(context, payload) {  // 1. payload接收dispatch传递过来的参数
       context.commit(INCREMENT_N, payload)  // 2. payload值为{num:10}，提交给mutation
+    },
+    // 8. 编写一个获取uuid的action
+    getUUIDAction({commit}) {
+      // 9. 直接返回Promise对象，作为dispatch函数的返回值
+      return new Promise((resolve, reject) => {
+        // 9.1. 发起网络请求，该URL是一个免费获取uuid的接口
+        fetch('https://httpbin.org/uuid')
+        // 9.2. 将res解析为JSON格式的Promise对象
+        .then(res => res.json())
+        .then((date) => {
+          // 9.3. 将请求获取的数据存到Vuex中
+          commit('addUUID', data.uuid)
+          // 9.4. 调用resolve完成异步操作
+          resolve(data)
+        }).catch((err) => {
+          reject(err) // 9.5. 错误处理
+        })
+      })
     }
   }
 })
