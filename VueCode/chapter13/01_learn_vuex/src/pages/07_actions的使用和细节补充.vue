7f4a26890cbf49be0c45fa5544517129d800f5b6
hushuhan <873933169@qq.com>
2024-05-05 23:38:13 +0800

    actions的基本使用

---
+++ VueCode/chapter13/01_learn_vuex/src/pages/07_actions的使用和细节补充.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="actions-use">
+    <h4>当前计数：{{ $store.state.counter }}</h4>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+export default {
+  methods: {
+    increment() { // Options API 语法
+      this.$store.dispatch("incrementAction") // 分发action
+    }
+  },
+  setup() {
+    // 2. setup语法
+    const store = useStore()
+    const decrement = () => {
+      store.dispatch("decrementAction") // 分发action
+    }
+    return { decrement }
+  }
+}
+</script>
+
+<style scoped></style>
