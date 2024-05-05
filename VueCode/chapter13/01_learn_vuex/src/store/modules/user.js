6dc4be148129c744a4585c89ef8938048363b9b5
hushuhan <873933169@qq.com>
2024-05-06 00:24:59 +0800

    modules的局部状态

--- VueCode/chapter13/01_learn_vuex/src/store/modules/user.js
+++ VueCode/chapter13/01_learn_vuex/src/store/modules/user.js
@@ -1,12 +1,31 @@
 const userModule = {
+  // 1. state属于user模块的状态，称为局部状态
   state() { // user模块的state
     return {
       // 在user模块中定义一个userCounter全局变量
       userCounter: 1000
     }
   },
-  getters: {},
-  mutations: {},
-  actions: {}
+  getters: {
+    // 2. user模块的mutation和getter函数接收的第一个参数是局部状态(state)
+    doubleUserCount(state) {
+      return state.userCounter * 2
+    }
+    // 4. user模块的getter函数，根节点状态（rootState）作为第三个参数暴露出来
+    userCountAddRootCount(state, getters, rootState) {
+      return state.userCounter + rootState.counter
+    }
+  },
+  mutations: {
+    increment(state) {  // 2. state是user模块的局部状态
+      state.userCounter++
+    }
+  },
+  actions: {
+    // 3. 在action函数中，局部状态通过context.state暴露出来，根节点状态通过context.rootState暴露出来
+    incrementAction({state, commit, rootState}) {
+      commit("increment")
+    }
+  }
 }
 export default userModule
