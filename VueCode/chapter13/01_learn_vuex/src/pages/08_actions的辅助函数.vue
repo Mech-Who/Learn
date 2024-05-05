1cb32aa9bcad168ef9e8670a22b0ee2bce585276
hushuhan <873933169@qq.com>
2024-05-05 23:46:25 +0800

    mapActions辅助函数

---
+++ VueCode/chapter13/01_learn_vuex/src/pages/08_actions的辅助函数.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h4>当前计数：{{ $store.state.counter }}</h4>
+    <button @click="incrementAction">+1</button>
+    <button @click="decrement">-1</button>
+    <button @click="addTen({num:20})">+10</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  methods: {
+    // 1. 将this.incrementAction()映射为this.$store.dispatch("incrementAction")
+    ...mapActions(['incrementAction'])  // Options API语法
+  },
+  setup() {
+    const actionsFuncs = mapActions({
+      decrement: "decrementAction",
+      addTen: "incrementNAction"
+    })
+    return {...actionsFuncs}
+  }
+}
+</script>
+
+<style scoped>
+</style>
