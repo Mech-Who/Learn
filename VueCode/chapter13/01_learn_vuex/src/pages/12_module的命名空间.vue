44cd13180918c5dc5440231e1c6abe92846c7b93
hushuhan <873933169@qq.com>
2024-05-06 20:56:54 +0800

    modules命名空间

---
+++ VueCode/chapter13/01_learn_vuex/src/pages/12_module的命名空间.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h4>home子模块homeCounter的状态： {{ $store.state.home.homeCounter }}</h4>
+    <h4>home子模块doubleHomeCount： {{ $store.getters['home/doubleHomeCount'] }}</h4>
+    <h4>home子模块doubleHomeCount： {{ $store.getters['home/homeCountAddRootCount'] }}</h4>
+    <button @click="incrementAction">+1</button>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+export default {
+  setup() {
+    const store = useStore()
+    const incrementAction = () => {
+      store.dispatch('home/incrementAction')  // 1. 只会触发home模块的incrementAction函数
+    }
+    return { incrementAction }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- VueCode/chapter13/01_learn_vuex/src/store/modules/home.js
+++ VueCode/chapter13/01_learn_vuex/src/store/modules/home.js
@@ -1,4 +1,5 @@
 const homeModule = {
+  namespaced: true, // 1. 为home模块添加命名空间，其它代码和user模块一样
   state() { // home模块的state
     return {
       // 在home模块中定义一个homeCounter全局变量
