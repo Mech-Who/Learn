77e8944ea2d575edf3714f8f9f7cae2bb804b23b
hushuhan <873933169@qq.com>
2024-05-06 21:05:59 +0800

    modules辅助函数：方式一：映射时指定模块名前缀

---
+++ VueCode/chapter13/01_learn_vuex/src/pages/13_modules辅助函数.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h4>home子模块homeCounter的状态： {{ homeCounter }}</h4>
+    <h4>home子模块doubleHomeCounter： {{ doubleHomeCounter }}</h4>
+    <button @click="homeIncrementCommit">+1</button>
+    <button @click="incrementAction">+1</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapMutations, mapActions } from 'vuex';
+export default {
+  computed: { // 方式一
+    ...mapState({
+      homeCounter: state => state.home.homeCounter
+    }),
+    ...mapGetters({
+      doubleHomeCount: "home/doubleHomeCount"
+    })
+  },
+  methods: {  // 方式一
+    ...mapMutations({
+      homeIncrementCommit: "home/increment"
+    }),
+    ...mapActions({
+      incrementAction: "home/incrementAction"
+    })
+  }
+}
+</script>
+
+<style scoped></style>
