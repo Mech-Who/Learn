6355270da0572648aad565b04b1359eaf654a7f2
hushuhan <873933169@qq.com>
2024-05-13 21:39:26 +0800

    添加构造函数

--- VueCode/chapter15/01_learn_typescript_advanced/01_TypeScript类的使用/01_类的定义.ts
+++ VueCode/chapter15/01_learn_typescript_advanced/01_TypeScript类的使用/01_类的定义.ts
@@ -1,14 +1,15 @@
-// 1. 定义一个person类
 class Person {
-  // 2. 定义属性，需初始化，否则编译报错
-  name: string = 'coder'
-  age: number = 18
-  // 3. 定义方法
+  name: string
+  age: number
+  // 1. 添加构造器，对属性进行初始化
+  constructor(name: string, age: number) {
+    this.name = name  // 属性初始化
+    this.age = age
+  }
   eating() { console.log(this.name + " eating") }
 }
 
-
-const p = new Person()  // 4. 新建一个类
+const p = new Person("why", 18)  // 4. 新建一个类
 console.log(p.name, p.age) // 5. 访问对象的属性
 p.eating()
 
