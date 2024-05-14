159725be0f0ecd32b03f694e7bd4ef4a24ccfda6
hushuhan <873933169@qq.com>
2024-05-14 23:06:25 +0800

    main: Updated 只读属性补充

--- VueCode/chapter15/01_learn_typescript_advanced/01_TypeScript类的使用/06_属性的只读-readonly.ts
+++ VueCode/chapter15/01_learn_typescript_advanced/01_TypeScript类的使用/06_属性的只读-readonly.ts
@@ -1,6 +1,8 @@
 type FriendType = { name: string }
 class Person {
+  // 1. 只读属性可以在构造器中赋值，但是赋值之后不可以修改。
   readonly name: string
+  // 2. 属性本身不能进行修改，但如果他是对象类型的，则对象中的属性可以修改。
   readonly friend?: FriendType
   constructor(name: string, friend?: FriendType) {
     this.name = name
@@ -8,10 +10,13 @@ class Person {
   }
 }
 
-const p = new Person("why", {name: 'kobe'})
+const p = new Person("why", { name: 'kobe' })
+// 3. 直接修改只读的name会报错：Cannot assign to 'name' because it is a read-only
+// p.name = 'liujun'
 console.log(p.name, p.friend)
 
+// p.friend = { name: 'evan' }  // 4. 直接修改只读的friend会报错
 if (p.friend) {
-  p.friend.name = 'evan'
+  p.friend.name = 'evan'  // 5. friend对象中的name属性是可以修改的
 }
 
