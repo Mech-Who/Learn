b8f64252c9c281091be97eb0a86a47aa71d86a21
hushuhan <873933169@qq.com>
2024-05-14 23:14:31 +0800

    抽象类abstract

---
+++ VueCode/chapter15/01_learn_typescript_advanced/01_TypeScript类的使用/09_抽象类abstract.ts
@@ -0,0 +1,39 @@
+// 1. 抽象类Shape
+abstract class Shape {
+  abstract getArea(): number // 2. 抽象方法，没有具体实现
+}
+
+class Rectangle extends Shape { // 3. 继承抽象类
+  private width: number
+  private height: number
+  constructor(width: number, height: number) {
+    super() // 在类的继承中，构造器必须调用super函数
+    this.width = width
+    this.height = height
+  }
+  getArea(){  // 4. 实现抽象类中的getArea抽象方法
+    return this.width * this.height
+  }
+}
+
+class Circle extends Shape {
+  private r: number
+  constructor(r: number) {
+    super()
+    this.r = r
+  }
+  getArea() { // 5. 实现抽象类中的getArea抽象方法
+    return this.r * this.r * 3.14
+  }
+}
+
+function makeArea(shape: Shape) {
+  return shape.getArea()  // 6. 多态的应用
+}
+
+const rectangle = new Rectangle(20, 30)
+const circle = new Circle(10)
+console.log(makeArea(rectangle));
+console.log(makeArea(circle));
+
+export {}
