a624ee0438f4786a42fa66123f9d081258e947fa
hushuhan <873933169@qq.com>
2024-05-16 23:53:15 +0800

    main: Added declare声明变量

--- VueCode/chapter15/01_learn_typescript_advanced/06_typescript_declare/src/main.ts
+++ VueCode/chapter15/01_learn_typescript_advanced/06_typescript_declare/src/main.ts
@@ -2,3 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+// 1. 使用全局变量
+// 在shims-vue.d.ts中声明后就可以正常使用了
+console.log(appName); // 报错：Cannot find name 'appName'.
+console.log(appVersion); // 报错：Cannot find name 'appVersion'.
--- VueCode/chapter15/01_learn_typescript_advanced/06_typescript_declare/src/shims-vue.d.ts
+++ VueCode/chapter15/01_learn_typescript_advanced/06_typescript_declare/src/shims-vue.d.ts
@@ -4,3 +4,7 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+// 1. 声明全局变量，告诉编译器该变量已经声明了
+declare const appName: string
+declare const appVersion: string
