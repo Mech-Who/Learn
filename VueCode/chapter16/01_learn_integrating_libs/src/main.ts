6b1e9d437dedd39301bd31bcd1d2bc704099a4cc
hushuhan <873933169@qq.com>
2024-05-18 11:24:14 +0800

    ELementPlus的按需引入

--- VueCode/chapter16/01_learn_integrating_libs/src/main.ts
+++ VueCode/chapter16/01_learn_integrating_libs/src/main.ts
@@ -4,12 +4,12 @@ import router from './router/index'
 import store from './store/index'
 
 // 全局引入 Element Plus 组件库
-import ElementPlus from 'element-plus'
+// import ElementPlus from 'element-plus'
 // 全局引入 Element Plus 组件库的样式
-import 'element-plus/dist/index.css'
+// import 'element-plus/dist/index.css'
 
 createApp(App)
   .use(router)
   .use(store)
-  .use(ElementPlus) // 3. 安装Element Plus组件库
+  // .use(ElementPlus) // 3. 安装Element Plus组件库
   .mount('#app')
--- VueCode/chapter16/01_learn_integrating_libs/vue.config.js
+++ VueCode/chapter16/01_learn_integrating_libs/vue.config.js
@@ -1,4 +1,28 @@
 const { defineConfig } = require('@vue/cli-service')
+// 1. 按需自动导入组件的插件
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    // 2. 为webpack添加两个插件
+    plugins: [
+      // 新版用法
+      // require('unplugin-auto-import/webpack').default({
+      //   resolvers: [ElementPlusResolver()]
+      // })
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      // 新版用法
+      // require('unplugin-vue-components/webpack').default({
+      //   resolvers: [ElementPlusResolver()]
+      // })
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  }
 })
