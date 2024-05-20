f31edd922e1692775af57c5250469d20ae2e4db9
HuShuhan <873933169@qq.com>
2024-05-20 16:01:48 +0800

    main: Added dictConfig配置的使用

---
+++ pythonLearn/LearnLogging/use_dict_config.py
@@ -0,0 +1,20 @@
+import logging
+import logging.config
+import yaml
+
+yaml_config = None
+with open("logging.yaml", 'r') as f:
+    yaml_config = yaml.safe_load(f)
+
+# print(yaml_config)
+
+logging.config.dictConfig(yaml_config)
+
+logger = logging.getLogger(__name__)
+
+# 'application' code
+logger.debug('debug message')
+logger.info('info message')
+logger.warning('warn message')
+logger.error('error message')
+logger.critical('critical message')
