8201b02f14bc4aef299fef88708322f323505419
HuShuhan <873933169@qq.com>
2024-05-20 16:18:57 +0800

    main: Added 配置过滤器filter

--- pythonLearn/LearnLogging/use_dict_config.py
+++ pythonLearn/LearnLogging/use_dict_config.py
@@ -2,6 +2,16 @@ import logging
 import logging.config
 import yaml
 
+
+def filter_maker(level):
+    level = getattr(logging, level)
+
+    def filter(record):
+        return record.levelno <= level
+
+    return filter
+
+
 yaml_config = None
 with open("logging.yaml", 'r') as f:
     yaml_config = yaml.safe_load(f)
@@ -10,7 +20,7 @@ with open("logging.yaml", 'r') as f:
 
 logging.config.dictConfig(yaml_config)
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("use_dict_config")
 
 # 'application' code
 logger.debug('debug message')
