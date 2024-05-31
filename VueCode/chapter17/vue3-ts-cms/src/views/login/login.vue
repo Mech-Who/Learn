2174490492b3fe2a6aaee63b0d1d256fc9697c08
hushuhan <873933169@qq.com>
2024-05-31 22:53:59 +0800

    main: Updated 删除默认组价；新建页面组件

---
+++ VueCode/chapter17/vue3-ts-cms/src/views/login/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">
+    <h4>Login Page</h4>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
---
+++ VueCode/chapter17/vue3-ts-cms/src/views/main/main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main-page">
+    <h4>Main Page</h4>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
