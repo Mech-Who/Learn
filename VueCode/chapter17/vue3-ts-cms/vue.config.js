f7a0b39c7c1a63e828e1543ba850654bc5ec232e
hushuhan <873933169@qq.com>
2024-05-31 22:01:25 +0800

    main: Updated vue.config.js中修改webpack配置

--- VueCode/chapter17/vue3-ts-cms/vue.config.js
+++ VueCode/chapter17/vue3-ts-cms/vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 方式一：使用Vue CLI提供的属性
+  outputDir: './build', // 应用打包输出的目录
+  publicPath: '/', // 应用打包时的基本URL
+
+  // 方式二：和webpack属性完全一致，最后与webpack的配置进行合并
+  configureWebpack: {
+    // 配置别名
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Vue CLI 5.x后默认已配置
+      components: '@/components'
+    }
+  }
 })
