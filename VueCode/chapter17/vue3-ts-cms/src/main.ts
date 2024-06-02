1cf98555cfc7b4cd29960db2a302899e3ce3377d
hushuhan <873933169@qq.com>
2024-06-02 23:49:25 +0800

    main: Added 搭建登录页面 第一步：重置页面样式和编写全局样式

--- VueCode/chapter17/vue3-ts-cms/src/main.ts
+++ VueCode/chapter17/vue3-ts-cms/src/main.ts
@@ -4,6 +4,8 @@ import router from './router'
 import store from './store'
 // 导入hyRequest实例
 import hyRequest from './service'
+import 'normalize.css'
+import './assert/css/index.less'
 
 createApp(App).use(store).use(router).mount('#app')
 
