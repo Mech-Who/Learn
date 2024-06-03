214cf0b6ded7f6c46329d8c9942f11d9f2cf4d07
hushuhan <873933169@qq.com>
2024-06-03 23:45:27 +0800

    main: Added 搭建登录页面 第二步：编写登录页面

--- VueCode/chapter17/vue3-ts-cms/src/main.ts
+++ VueCode/chapter17/vue3-ts-cms/src/main.ts
@@ -4,10 +4,16 @@ import router from './router'
 import store from './store'
 // 导入hyRequest实例
 import hyRequest from './service'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'normalize.css'
 import './assets/css/index.less'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+// 从 @element-plus/icons-vue 中导入所有图标，并进行全局注册
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(store).use(router).mount('#app')
 
 console.log(process.env.NODE_ENV) // development
 console.log(process.env.VUE_APP_BASE_URL) // https://coderwhy/org/dev
