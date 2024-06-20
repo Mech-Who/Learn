0d3a244d6628194a9c1c0d2cb650b7767d5ba03c
hushuhan <873933169@qq.com>
2024-06-20 21:24:18 +0800

    main: Added 搭建首页布局

--- VueCode/chapter17/vue3-ts-cms/src/views/main/main.vue
+++ VueCode/chapter17/vue3-ts-cms/src/views/main/main.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="main-page">
-    <h4>Main Page</h4>
+  <div class="main-content">
+    <el-container class="main-content">
+      <el-aside :width="'210px'">左边菜单</el-aside>
+      <el-container class="page">
+        <el-header class="page-header">
+          <!-- nav header -->
+        </el-header>
+        <el-main class="page-content">Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -12,3 +20,59 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped lang="less">
+.main {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.main-content,
+.page {
+  height: 100%;
+}
+
+.page-content {
+  height: calc(100% - 48px);
+}
+
+.el-header,
+.el-footer {
+  display: flex;
+  color: #333;
+  text-align: center;
+  align-items: center;
+}
+
+.el-header {
+  height: 48px !important;
+}
+
+.el-aside {
+  overflow-x: hidden;
+  overflow-y: auto;
+  line-height: 200px;
+  text-align: left;
+  cursor: pointer;
+  background-color: #001529;
+  transition: width 0.3s linear;
+  scrollbar-width: none;
+  /* firefox */
+  -ms-overflow-style: none;
+  /* IE 10+ */
+
+  &::-webkit-scrollbar {
+    display: none;
+    /* 隐藏侧边栏布局 */
+  }
+}
+
+.el-main {
+  color: #333;
+  text-align: center;
+  background-color: #f0f2f5;
+}
+</style>
