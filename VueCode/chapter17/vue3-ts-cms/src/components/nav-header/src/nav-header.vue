b86569c4c9034d25a3af34911fc685031359567f
hushuhan <873933169@qq.com>
2024-06-24 22:10:14 +0800

    main: Added 封装头部栏组件

---
+++ VueCode/chapter17/vue3-ts-cms/src/components/nav-header/src/nav-header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="nav-header">
+    <el-icon class="fold-menu" @click="handleFoldClick">
+      <component :is="isFold ? 'expand' : 'fold'"></component>
+    </el-icon>
+    <div class="content">
+      <div>面包屑</div>
+      <user-info />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import UserInfo from './user-info.vue'
+
+const isFold = ref(false)
+const emit = defineEmits(['foldChange'])
+const handleFoldClick = () => {
+  isFold.value = !isFold.value
+  emit('foldChange', isFold.value)
+}
+
+defineExpose({ isFold, handleFoldClick })
+</script>
+
+<style scoped lang="less">
+.nav-header {
+  display: flex;
+  width: 100%;
+
+  .fold-menu {
+    font-size: 30px;
+    cursor: pointer;
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 20px;
+  }
+}
+</style>
