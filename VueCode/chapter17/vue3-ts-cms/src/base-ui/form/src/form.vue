b0adf62922150cef183edfc3c0d6d1832792cdac
hushuhan <873933169@qq.com>
2024-06-24 23:59:22 +0800

    main: Updated form.vue优化

--- VueCode/chapter17/vue3-ts-cms/src/base-ui/form/src/form.vue
+++ VueCode/chapter17/vue3-ts-cms/src/base-ui/form/src/form.vue
@@ -3,27 +3,52 @@
     <div class="header"><slot name="header"></slot></div>
     <el-form label-width="labelWidth">
       <el-row>
-        <el-col v-bind="colLayout">
-          <el-form-item label="用户名">
-            <el-input
-              :model-value="modelValue.enable"
-              placeholder="选择状态"
-              @update:modelValue="handleValueChange($event, 'enable')"
-            />
-          </el-form-item>
-        </el-col>
-        <el-col v-bind="colLayout">
-          <el-form-item label="状态">
-            <el-select
-              :model-value="modelValue.enable"
-              placeholder="选择状态"
-              @update:modelValue="handleValueChange($event, 'enable')"
+        <template v-for="item in formItems" :key="item.label">
+          <el-col v-bind="colLayout">
+            <el-form-item
+              v-if="!item.isHidden"
+              :label="item.label"
+              :rules="item.rules"
+              :style="itemStyle"
             >
-              <el-option label="启动" value="1" />
-              <el-option label="禁用" value="0" />
-            </el-select>
-          </el-form-item>
-        </el-col>
+              <template
+                v-if="item.type === 'input' || item.type === 'password'"
+              >
+                <el-input
+                  :placeholder="item.placeholder"
+                  v-bind="item.otherOptions"
+                  :show-password="item.type === 'password'"
+                  :model-value="modelValue[`${item.field}`]"
+                  @update:modelValue="handleValueChange($event, item.field)"
+                />
+              </template>
+              <template v-else-if="item.type === 'select'">
+                <el-select
+                  :placeholder="item.placeholder"
+                  v-bind="item.otherOptions"
+                  style="width: 100%"
+                  :model-value="modelValue[`${item.field}`]"
+                  @update:modelValue="handleValueChange($event, item.field)"
+                >
+                  <el-option
+                    v-for="option in item.options"
+                    :key="option.value"
+                    :value="option.value"
+                    >{{ option.title }}</el-option
+                  >
+                </el-select>
+              </template>
+              <template v-else-if="item.type === 'datepicker'">
+                <el-data-picker
+                  style="width: 100%"
+                  v-bind="item.otherOptions"
+                  :model-value="modelValue[`${item.field}`]"
+                  @update:modelValue="handleValueChange($event, item.field)"
+                ></el-data-picker>
+              </template>
+            </el-form-item>
+          </el-col>
+        </template>
       </el-row>
     </el-form>
     <div class="footer"><slot name="footer"></slot></div>
@@ -31,16 +56,22 @@
 </template>
 
 <script lang="ts" setup>
+import { IFormItem } from '../types'
+
 interface Props {
   modelValue: any
   labelWidth?: string
   colLayout?: any
+  itemStyle?: any
+  formItems: IFormItem[]
 }
 
 const props = withDefaults(defineProps<Props>(), {
   modelValue: () => ({}),
   labelWidth: '100px',
-  colLayout: () => ({ xl: 6, lg: 8, md: 12, sm: 24, xs: 24 })
+  colLayout: () => ({ xl: 6, lg: 8, md: 12, sm: 24, xs: 24 }),
+  itemStyle: () => ({ padding: '10px 40px' }),
+  formItems: () => []
 })
 
 const emit = defineEmits<{
