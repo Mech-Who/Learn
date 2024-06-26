14df8c1dad376927f1d64390d8f93754e0686537
hushuhan <873933169@qq.com>
2024-06-26 22:32:14 +0800

    main: Added 日期格式化

--- VueCode/chapter17/vue3-ts-cms/src/utils/date-format.ts
+++ VueCode/chapter17/vue3-ts-cms/src/utils/date-format.ts
@@ -1,3 +1,5 @@
-export function formatTime(date: any) {
-  return `${date}`
+export function formatTime(utcString: string) {
+  const d = new Date(utcString)
+  const date = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate()
+  return date
 }
