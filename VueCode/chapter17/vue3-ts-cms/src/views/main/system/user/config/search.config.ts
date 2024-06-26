1aebfa7ef382ad1fa2f24a012cc9f02c5929df82
hushuhan <873933169@qq.com>
2024-06-26 21:55:19 +0800

    main: Added 查询配置添加

--- VueCode/chapter17/vue3-ts-cms/src/views/main/system/user/config/search.config.ts
+++ VueCode/chapter17/vue3-ts-cms/src/views/main/system/user/config/search.config.ts
@@ -9,6 +9,24 @@ export const searchFormConfig: IForm = {
       placeholder: '请输入id'
     },
     {
+      field: 'name',
+      type: 'input',
+      label: '用户名',
+      placeholder: '请输入用户名'
+    },
+    {
+      field: 'realname',
+      type: 'input',
+      label: '真实姓名',
+      placeholder: '请输入真实姓名'
+    },
+    {
+      field: 'cellphone',
+      type: 'input',
+      label: '电话号码',
+      placeholder: '请输入电话号码'
+    },
+    {
       field: 'enable',
       type: 'select',
       label: '用户状态',
