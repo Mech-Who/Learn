dfd43b45848da2bacc9d8f8c9fa333e598cdc9a7
hushuhan <873933169@qq.com>
2024-06-26 21:03:48 +0800

    main: Added 用户列表页面编写

---
+++ VueCode/chapter17/vue3-ts-cms/src/components/page-content/src/page-content.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="page-content">
+    <hy-table
+      :listData="dataList"
+      :listCount="dataCount"
+      v-model:page="pageInfo"
+    >
+      <template #headerHandler>
+        <el-button type="primary" @click="handleNewClick"> 新建用户 </el-button>
+      </template>
+    </hy-table>
+    <h4>Hello World1</h4>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import HyTable from '@/base-ui/table'
+import { computed, ref } from 'vue'
+interface Props {
+  pageName: string
+}
+
+const props = withDefaults(defineProps<Props>(), { pageName: '' })
+const pageInfo = ref({ currentPage: 1, pageSize: 10 })
+const dataList = computed(() => {
+  return [{ name: 'coderwhy', enable: 1, pageSize: 10 }]
+})
+const dataCount = 0
+const emit = defineEmits<{
+  (e: 'newBtnClick'): void
+}>()
+const handleNewClick = () => {
+  emit('newBtnClick')
+}
+</script>
+
+<style scoped></style>
