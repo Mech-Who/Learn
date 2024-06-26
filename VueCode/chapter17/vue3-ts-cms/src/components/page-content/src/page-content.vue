15c6c399341b4e20427560b076373790d60bbef2
hushuhan <873933169@qq.com>
2024-06-26 21:48:21 +0800

    main: Updated 页面逻辑修改

--- VueCode/chapter17/vue3-ts-cms/src/components/page-content/src/page-content.vue
+++ VueCode/chapter17/vue3-ts-cms/src/components/page-content/src/page-content.vue
@@ -15,23 +15,46 @@
 
 <script lang="ts" setup>
 import HyTable from '@/base-ui/table'
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
+import { useStore } from 'vuex'
+
 interface Props {
   pageName: string
 }
 
 const props = withDefaults(defineProps<Props>(), { pageName: '' })
+
+const store = useStore()
+
 const pageInfo = ref({ currentPage: 1, pageSize: 10 })
-const dataList = computed(() => {
-  return [{ name: 'coderwhy', enable: 1, pageSize: 10 }]
-})
-const dataCount = 0
+watch(pageInfo, () => getPageData())
+const getPageData = (queryInfo: any = {}) => {
+  store.dispatch('system/getPageListAction', {
+    pageName: props.pageName,
+    queryInfo: {
+      offset: (pageInfo.value.currentPage - 1) * pageInfo.value.pageSize,
+      size: pageInfo.value.pageSize,
+      ...queryInfo
+    }
+  })
+}
+
+getPageData()
+
+const dataList = computed(() =>
+  store.getters[`system/pageListData`](props.pageName)
+)
+const dataCount = computed(() =>
+  store.getters[`system/pageListCount`](props.pageName)
+)
 const emit = defineEmits<{
   (e: 'newBtnClick'): void
 }>()
 const handleNewClick = () => {
   emit('newBtnClick')
 }
+
+defineExpose({ getPageData })
 </script>
 
 <style scoped></style>
--- VueCode/chapter17/vue3-ts-cms/src/views/main/system/user/user.vue
+++ VueCode/chapter17/vue3-ts-cms/src/views/main/system/user/user.vue
@@ -5,23 +5,31 @@
       @resetBtnClick="handleResetClick"
       @queryBtnClick="handleQueryClick"
     ></page-search>
-    <page-content pageName="users" @newBtnClick="handleNewData"></page-content>
+    <page-content
+      ref="pageContentRef"
+      pageName="users"
+      @newBtnClick="handleNewData"
+    ></page-content>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
 import PageSearch from '@/components/page-search'
 import PageContent from '@/components/page-content'
 import { searchFormConfig } from './config/search.config'
 
-const handleResetClick = () => {
-  console.log('handleResetClick')
-}
-const handleQueryClick = (formData: any) => {
-  console.log('handleQueryClick', formData)
-}
 const handleNewData = () => {
   console.log('handleNewData')
+}
+
+const pageContentRef = ref<InstanceType<typeof PageContent>>()
+
+const handleResetClick = () => {
+  pageContentRef.value?.getPageData()
+}
+const handleQueryClick = (formData: any) => {
+  pageContentRef.value?.getPageData(formData)
 }
 </script>
 
