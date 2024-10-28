5fbf8e9455487fe1cc2d4f96b09f66e0ba7c8311
HuShuhan <873933169@qq.com>
2024-10-28 13:07:02 +0800

    二分查找优化

--- AlgorithmLearn/Cpp/704_二分查找.cpp
+++ AlgorithmLearn/Cpp/704_二分查找.cpp
@@ -1,18 +1,21 @@
+/*
+代码随想录：https://programmercarl.com/0704.%E4%BA%8C%E5%88%86%E6%9F%A5%E6%89%BE.html
+*/
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int right = nums.size()-1;
         int left = 0;
-        int i = (right + left) / 2;
         // cout << left << " " << right << " " << i << endl; // 调试信息
-        while(left <= right){ // point 1: 退出条件
+        while(left <= right){ // point 1: 退出条件，始终坚持根据查找区间的定义来做边界处理
+            int i = (right + left) / 2;
             if(nums[i] < target)
                 left = i+1; // point 2: 小了就从后一个开始
             else if(nums[i] > target)
                 right = i-1;// point 2: 大了就从前一个开始
             else
                 return i;
-            i = (right + left) / 2; // point 3: 更新指中索引
             // cout << left << " " << right << " " << i << endl; // 调试信息
         }
         return -1;
