3276bdf9bf03bfa3e4a4e8134a3bb6f1f8bc06de
HuShuhan <873933169@qq.com>
2024-10-31 13:20:22 +0800

    main函数参数调整

--- cppLearn/EssentialCpp/Chapter1/guess_number_v1.cpp
+++ cppLearn/EssentialCpp/Chapter1/guess_number_v1.cpp
@@ -2,7 +2,7 @@
  * @Author: HuShuhan 873933169@qq.com
  * @Date: 2024-10-31 12:49:59
  * @LastEditors: HuShuhan 873933169@qq.com
- * @LastEditTime: 2024-10-31 12:50:08
+ * @LastEditTime: 2024-10-31 13:20:15
  * @FilePath: \EssentialCpp\Chapter1\guess_number.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -21,7 +21,7 @@ vector<int> tri_seq{seq_size};
 vector<int> squa_seq{seq_size};
 vector<int> penta_seq{seq_size};
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
     int user_guess;
     int first_num = 1;
--- cppLearn/EssentialCpp/Chapter1/guess_number_v2.cpp
+++ cppLearn/EssentialCpp/Chapter1/guess_number_v2.cpp
@@ -2,7 +2,7 @@
  * @Author: HuShuhan 873933169@qq.com
  * @Date: 2024-10-31 12:49:59
  * @LastEditors: HuShuhan 873933169@qq.com
- * @LastEditTime: 2024-10-31 12:50:08
+ * @LastEditTime: 2024-10-31 13:18:30
  * @FilePath: \EssentialCpp\Chapter1\guess_number.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -21,7 +21,7 @@ vector<int> tri_seq{seq_size};
 vector<int> squa_seq{seq_size};
 vector<int> penta_seq{seq_size};
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
     int user_guess;
     int first_num = 1;
@@ -36,21 +36,24 @@ int main(int argc, char *argv)
         ++guess_count;
         if (user_guess == 2)
         {
-            cout << "Congratulations! Nice guess! You're right!";
+            cout << "Congratulations! Nice guess! You're right!\n";
         }
         else
         {
             cout << "Sorry, nice try, but the answer is wrong!\n";
         }
         char continue_choice;
-        cout << "Still wanna try another guess? Y/N";
+        cout << "Still wanna try another guess? Y/N\n";
         cin >> continue_choice;
-        switch(continue_choice) {
-            case 'y':case 'Y':
-                break;
-            case 'n':case 'N':
-                still_guess = false;
-                break;
+        switch (continue_choice)
+        {
+        case 'y':
+        case 'Y':
+            break;
+        case 'n':
+        case 'N':
+            still_guess = false;
+            break;
         }
     }
 }
