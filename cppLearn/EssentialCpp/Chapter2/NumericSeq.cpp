1790f9b999238663fd5a626b8063e8803153cff4
hushuhan <873933169@qq.com>
2024-11-03 15:01:45 +0800

    函数指针、函数指针数组、枚举类型

--- cppLearn/EssentialCpp/Chapter2/NumericSeq.cpp
+++ cppLearn/EssentialCpp/Chapter2/NumericSeq.cpp
@@ -1,33 +1,64 @@
 #include "NumericSeq.h"
 
-void display(const vector<int> &vec, ostream &os){
-    for (int ix=0; ix<vec.size(); ++ix){
+void display(const vector<int> &vec, ostream &os)
+{
+    for (int ix = 0; ix < vec.size(); ++ix)
+    {
         os << vec[ix] << ' ';
     }
     os << endl;
 }
 
-const vector<int>*
+const vector<int> *
 fibon_seq(int size)
 {
     static vector<int> elems;
 
-    if (! is_size_ok(size))
+    if (!is_size_ok(size))
         return nullptr;
 
-    for (int ix=elems.size(); ix < size; ++ix){
-        if ( ix==0 || ix==1)
+    for (int ix = elems.size(); ix < size; ++ix)
+    {
+        if (ix == 0 || ix == 1)
             elems.push_back(1);
         else
-            elems.push_back(elems[ix-1] + elems[ix-2]);
+            elems.push_back(elems[ix - 1] + elems[ix - 2]);
     }
     return &elems;
 }
 
-template <typename elemType>
-void display_message(const string&, const vector<elemType> &vec, ostream& os){
+const vector<int> *
+penta_seq(int size){
+    static vector<int> elems;
+
+    if (is_size_ok(size))
+        return nullptr;
+
+    for (int ix=elems.size(); ix < size; ++ix){
+        int n = ix+1;
+        elems[ix] = n * (3 * n - 1) / 2;
+    }
+    return &elems;
+}
+
+void display_message(char ch, ostream &os)
+{
+    os << ch;
+}
+void display_message(const string &msg, ostream &os)
+{
     os << msg;
-    for ( int ix=0; ix < vec.size(); ++ix ){
+}
+void display_message(const string &msg, int elem, ostream &os){
+    os << msg << "elem:" << elem;
+}
+
+template <typename elemType>
+void display_message(const string &msg, const vector<elemType> &vec, ostream &os)
+{
+    os << msg;
+    for (int ix = 0; ix < vec.size(); ++ix)
+    {
         elemType t = vec[ix];
         os << t << ' ';
     }
--- cppLearn/EssentialCpp/Chapter2/NumericSeq.h
+++ cppLearn/EssentialCpp/Chapter2/NumericSeq.h
@@ -6,36 +6,59 @@ using namespace std;
 
 void display(const vector<int> &vec, ostream &os = cout);
 
-inline bool fibon_elem(int pos, int &elem){
-    const vector<int> *pseq = fibon_seq(pos);
+enum ns_type
+{
+    ns_fibon,
+    ns_penta
+};
 
-    if (!pseq){
+const vector<int> *(*seq_array[])(int) = {
+    fibon_seq, penta_seq};
+
+inline bool seq_elem(int pos, int &elem,
+                     const vector<int> *(*seq_ptr)(int) = nullptr)
+{
+    if (!seq_ptr)
+    {
+        const string msg("Internal Error: seq_ptr is set to null!");
+        display_message(msg);
         elem = 0;
         return false;
     }
-    elem = (*pseq)[pos-1];
+
+    const vector<int> *pseq = seq_ptr(pos);
+
+    if (!pseq)
+    {
+        const string msg("Internal Error: pseq is set to null!");
+        display_message(msg);
+        elem = 0;
+        return false;
+    }
+    elem = (*pseq)[pos - 1];
     return true;
 }
 
 const vector<int> *fibon_seq(int size);
+const vector<int> *penta_seq(int size);
 
-inline bool is_size_ok(int size) {
+inline bool is_size_ok(int size)
+{
     const int max_size = 1024;
-    if (size < 0 || size > max_size){
-        cerr << "fibon_seq(): oops: invalid size:"
-             << size << " -- can't fulfill request.\n";
+    const string msg("Requested size is not supported!");
+
+    if (size < 0 || size > max_size)
+    {
+        display_message(msg, size);
         return false;
     }
     return true;
 }
 
-void display_message(char ch);
-void display_message(const string&);
-void display_message(const string&, int);
-void display_message(const string&, int, int);
-
+void display_message(char, ostream &os = cout);
+void display_message(const string &, ostream &os = cout);
+void display_message(const string &, int, ostream &os = cout);
 template <typename elemType>
-void display_message(const string&, const vector<elemType> &vec, ostream& os=cout);
-
+void display_message(const string &, const vector<elemType> &, ostream &os = cout);
 template <typename elemType>
-void display_message(const string&, const list<elemType> &vec, ostream& os=cout);
+void display_message(const string &, const list<elemType> &, ostream &os = cout);
