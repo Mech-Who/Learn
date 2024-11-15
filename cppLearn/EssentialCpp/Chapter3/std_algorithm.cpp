e7b003dae161cceb0e3ccff436b22cd258f1cada
HuShuhan <873933169@qq.com>
2024-11-15 16:18:01 +0800

    .gitignore添加codearts配置文件；添加算法

---
+++ cppLearn/EssentialCpp/Chapter3/std_algorithm.cpp
@@ -0,0 +1,14 @@
+int *find(const vector<int> &vec, int value){
+    for(int ix=0;ix < vec.size(); ++ix)
+        if(vec[ix]==value)
+            return &vec[ix];
+    return 0;
+}
+
+template<typename elemType>
+elemType* find2(const vector<elemType> &vec, const elemType value){
+    for (int ix=0; ix < vec.size(); ++ix)
+        if (vec[ix]==value)
+            return &vec[ix];
+    return 0;
+}
