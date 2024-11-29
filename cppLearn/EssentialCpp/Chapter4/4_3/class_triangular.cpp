625cdebbfa8b1c00e82c813780f4c971431e0440
HuShuhan <873933169@qq.com>
2024-11-29 22:09:31 +0800

    重载运算符！

--- cppLearn/EssentialCpp/Chapter4/4_3/class_triangular.cpp
+++ cppLearn/EssentialCpp/Chapter4/4_3/class_triangular.cpp
@@ -1,5 +1,16 @@
-#include <iostream>
+/*
+ * @Author: HuShuhan 873933169@qq.com
+ * @Date: 2024-11-22 14:23:18
+ * @LastEditors: HuShuhan 873933169@qq.com
+ * @LastEditTime: 2024-11-29 22:09:15
+ * @FilePath: \EssentialCpp\Chapter4\4_3\class_triangular.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <algorithm>
+#include <iterator>
 #include "class_triangular.h"
+
+using namespace std;
 
 Triangular::Triangular(int len, int beg_pos)
     : _name("Triangular")
@@ -8,33 +19,187 @@ Triangular::Triangular(int len, int beg_pos)
     _beg_pos = beg_pos > 0 ? beg_pos : 1;
     _next = _beg_pos - 1;
 
-    _generate_elem(_beg_pos+_length-1);
+    _generate_elem(_beg_pos + _length - 1);
 }
 
 // 成员初始化列表，主要用于对是object的成员的赋值，基础类型则都可以
 Triangular::Triangular(const Triangular &rhs)
-    : _length(rhs._length), _beg_pos(rhs._beg_pos), _next(rhs._beg_pos-1)
+    : _length(rhs._length), _beg_pos(rhs._beg_pos), _next(rhs._beg_pos - 1)
 {
-    _generate_elem(_beg_pos+_length-1);
+    _generate_elem(_beg_pos + _length - 1);
 }
 
 Triangular::~Triangular()
-{}
-
-int Triangular::elem(int pos) const{
-    return _elems[pos-1];
+{
 }
 
-bool Triangular::next(int &val){
+int Triangular::elem(int pos) const
+{
+    return _elems[pos - 1];
+}
+
+bool Triangular::next(int &val)
+{
     if (_next >= _beg_pos + _length - 1)
         return false;
-    val = _elems[ _next++ ];
+    val = _elems[_next++];
     return true;
 }
 
-void Triangular::_generate_elem(int last_pos){
+Triangular &Triangular::operator=(const Triangular &rhs)
+{
+    if (this != &rhs)
+    {
+        _name = rhs._name;
+        _length = rhs._length;
+        _beg_pos = rhs._beg_pos;
+        _next = rhs._next;
+        copy(rhs._elems.begin(), rhs._elems.end(), _elems);
+    }
+}
+
+int Triangular::operator()(int index)
+{
+    if (index < _length)
+    {
+        return _elems[index];
+    }
+    else
+    {
+        // TODO: throw error();
+        return -1;
+    }
+}
+
+ostream &operator<<(ostream &os, Triangular &rhs)
+{
+    os << "( " << rhs._beg_pos << ", " << rhs._length << " )";
+    rhs.display(rhs._length, rhs._beg_pos, os);
+    return os;
+}
+
+istream &operator>>(istream &is, Triangular &rhs)
+{
+    is >> rhs._beg_pos >> rhs._length;
+    rhs.next_reset();
+    if (rhs._beg_pos + rhs._length > rhs._elems.size())
+    {
+        Triangular::gen_elements(rhs._beg_pos + rhs._length);
+    }
+    return is;
+}
+
+bool Triangular::is_elem(int value)
+{
+    if (!_elems.size() || _elems[_elems.size() - 1] < value)
+        gen_elems_to_value(value);
+
+    vector<int>::iterator found_it;
+    vector<int>::iterator end_it = _elems.end();
+
+    found_it = find(_elems.begin(), end_it, value);
+    return found_it != end_it;
+}
+
+void Triangular::gen_elements(int length)
+{
+    if (length < 0 || length > _max_elems)
+    {
+        // TODO: 发出错误消息
+        return;
+    }
+    if (_elems.size() < length)
+    {
+        int ix = _elems.size() ? _elems.size() + 1 : 1;
+        for (; ix <= length; ++ix)
+        {
+            _elems.push_back(ix * (ix + 1) / 2);
+        }
+    }
+}
+
+void Triangular::gen_elems_to_value(int value)
+{
+    int ix = _elems.size();
+    if (!ix)
+    {
+        _elems.push_back(1);
+        ix = 1;
+    }
+    while (_elems[ix - 1] < value && ix < _max_elems)
+    {
+        // cout << "elems to value: " << ix*(ix+1)/2 << endl;
+        ++ix;
+        _elems.push_back(ix * (ix + 1) / 2);
+    }
+    if (ix == _max_elems)
+        cerr << "Triangular Sequence: oops: value too large "
+             << value << " --  exceeds max size of "
+             << _max_elems << endl;
+}
+
+void Triangular::display(int length, int beg_pos, ostream &os)
+{
+    for (int ix = 0; ix < length; ++ix)
+    {
+        os << _elems[beg_pos + ix] << " ";
+    }
+    os << endl;
+}
+
+void Triangular::_generate_elem(int last_pos)
+{
     if (_elems.size() >= last_pos)
         return;
-    for (int ix=_elems.size(); ix < last_pos; ++ix)
-        _elems.push_back((ix+1)*(ix+2)/2);
+    for (int ix = _elems.size(); ix < last_pos; ++ix)
+        _elems.push_back((ix + 1) * (ix + 2) / 2);
+}
+
+inline bool Triangular::iterator::
+operator==(const Triangular_iterator &rhs) const
+{
+    return _index == rhs._index;
+}
+
+inline bool Triangular::iterator::
+operator!=(const Triangular_iterator &rhs) const
+{
+    return !(*this == rhs);
+}
+
+inline int Triangular::iterator::
+operator*() const
+{
+    this->check_integrity();
+    return Triangular::_elems[_index];
+}
+
+inline void Triangular::iterator::
+    check_integrity() const
+{
+    if (_index >= Triangular::_max_elems)
+    {
+        // TODO: 丢出异常
+        // throw iterator_overflow();
+        return;
+    }
+    if (_index >= Triangular::_elems.size())
+        Triangular::gen_elements(_index + 1);
+}
+
+inline Triangular::iterator &Triangular::iterator::
+operator++()
+{
+    ++_index;
+    check_integrity();
+    return *this;
+}
+
+inline Triangular::iterator Triangular::iterator::
+operator++(int)
+{
+    Triangular_iterator tmp = *this;
+    ++_index;
+    check_integrity();
+    return tmp;
 }
--- cppLearn/EssentialCpp/Chapter4/4_3/class_triangular.h
+++ cppLearn/EssentialCpp/Chapter4/4_3/class_triangular.h
@@ -2,7 +2,7 @@
  * @Author: HuShuhan 873933169@qq.com
  * @Date: 2024-11-22 14:23:18
  * @LastEditors: HuShuhan 873933169@qq.com
- * @LastEditTime: 2024-11-22 17:58:53
+ * @LastEditTime: 2024-11-29 21:59:17
  * @FilePath: \EssentialCpp\Chapter4\4_3\class_triangular.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -27,9 +27,46 @@ public:
     bool next(int &val);
     void next_reset() { _next = _beg_pos - 1; }
 
+    Triangular &operator=(const Triangular &rhs);
+    int operator()(int index);
+
+    friend std::ostream& operator<<(std::ostream& os, Triangular &rhs);
+    friend std::istream& operator>>(std::istream& is, Triangular &rhs);
+
+    static bool is_elem(int);
+    static void gen_elements(int length);
+    static void gen_elems_to_value(int value);
+    static void display(int length, int beg_pos, std::ostream &os = std::cout);
+
     // cout << Triangular()
     // friend operator<<(ostream& os){
     // }
+
+    class Triangular_iterator
+    {
+    public:
+        Triangular_iterator(int index) : _index(index - 1) {}
+        bool operator==(const Triangular_iterator &) const;
+        bool operator!=(const Triangular_iterator &) const;
+        int operator*() const;
+        Triangular_iterator &operator++();
+        Triangular_iterator operator++(int);
+
+        friend int operator*(const Triangular_iterator &rhs);
+
+    private:
+        void check_integrity() const;
+        int _index;
+    };
+
+    friend int operator*(const Triangular_iterator &rhs);
+    // friend int Triangular_iterator::operator*();
+    // friend void Triangular_iterator::check_integrity();
+
+    typedef Triangular_iterator iterator;
+
+    iterator begin() const { return iterator(_beg_pos); }
+    iterator end() const { return iterator(_beg_pos + _length); }
 
 private:
     static void _generate_elem(int last_pos);
@@ -40,5 +77,6 @@ private:
     // mutable 表示使用const Triangular(对象)时,改变_next不会造成const报错
     mutable int _next; // 下一个迭代目标
 
+    static const int _max_elems = 1024;
     static std::vector<int> _elems;
 };
